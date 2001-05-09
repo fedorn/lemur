56d0f694d65e2d4b6fd71da25611dbd29e880078
tnt <tnt>
2001-05-09 20:27:38 +0000

    file to take care of IFWIN differences

---
+++ utility/include/map.hpp
@@ -0,0 +1,16 @@
+#ifndef _MAP_H
+#define _MAP_H
+
+#include <map>
+
+#endif
+
+#ifndef _NAMESPACE
+#define _NAMESPACE
+
+#ifdef _WIN32
+using namespace std;
+#endif
+
+#endif
+
---
+++ utility/include/vector.hpp
@@ -0,0 +1,16 @@
+#ifndef _VECTOR_H
+#define _VECTOR_H
+
+#include <vector>
+
+#endif
+
+#ifndef _NAMESPACE
+#define _NAMESPACE
+
+#ifdef _WIN32
+using namespace std;
+#endif
+
+#endif
+
