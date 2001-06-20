8f68732ec37261c30f25ba4016fa792ee1f28df6
tnt <tnt>
2001-06-20 21:25:03 +0000

    added a couple const to match the virtual Index.hpp api added aveDocLen var added mechanism to handle multiple dt index files modified open to call mainToc which parses .toc file to get stats

--- index/src/InvFPIndex.cpp
+++ index/src/InvFPIndex.cpp
@@ -7,34 +7,79 @@
  *========================================================================*/
 InvFPIndex::InvFPIndex() {
   lookup = NULL;  
+  dtlookup = NULL;
+  aveDocLen = 0;
 }
 
-InvFPIndex::InvFPIndex(char* indexName) {
+InvFPIndex::InvFPIndex(const char* indexName) {
+  lookup = NULL;  
+  dtlookup = NULL;
+  aveDocLen = 0;
   open(indexName);
 }
 
 InvFPIndex::~InvFPIndex() {
   if (lookup != NULL)
     delete[](lookup);
+  if (dtlookup  != NULL)
+    delete[](dtlookup);
+  if (names != NULL) {
+    for (int i = 0; i < 6; i++) {
+      if (names[i] != NULL) {
+        delete[](names[i]);
+        names[i] = NULL;
+      }
+    }
+    delete[](names);
+  }
 }
 
 bool InvFPIndex::open(const char* indexName){
-  counts = new int[3];
-  counts[TOTAL_TERMS] = 315;
-  counts[UNIQUE_TERMS] = 174;
-  counts[DOCS] = 23;
-  names = new char*[6];
-  names[DOC_INDEX] = strcat((char*)indexName, INVINDEX);
-  names[DOC_LOOKUP] = strcat((char*)indexName, INVLOOKUP);
-  names[TERM_INDEX] = strcat((char*)indexName, DTINDEX);
-  names[TERM_LOOKUP] = strcat((char*)indexName, DTLOOKUP);
-  names[TERM_IDS] = strcat((char*)indexName, TERMIDMAP);
-  names[DOC_IDS] = strcat((char*)indexName, DOCIDMAP);
+  counts = new int[5];
+  names = new char*[6];  
+  int nameLen = strlen(indexName);
+
+  char * tocName = new char[nameLen + strlen(MAINTOC) + 1];
+  strncpy(tocName, indexName, nameLen);
+  strcpy(tocName + nameLen, MAINTOC);
+
+  if (!mainToc(tocName)) {
+    delete[](tocName);
+    return false;
+  }
+  delete[](tocName);
+
+  names[DOC_INDEX] = new char[nameLen + strlen(INVINDEX) + 1];
+  strncpy(names[DOC_INDEX], indexName, nameLen);
+  strcpy(names[DOC_INDEX] + nameLen, INVINDEX);
+
+  names[DOC_LOOKUP] = new char[nameLen + strlen(INVLOOKUP) + 1];
+  strncpy(names[DOC_LOOKUP], indexName, nameLen);
+  strcpy(names[DOC_LOOKUP] + nameLen, INVLOOKUP);
+
+  names[TERM_INDEX] = new char[nameLen + strlen(DTINDEX) + 1];
+  strncpy(names[TERM_INDEX], indexName, nameLen);
+  strcpy(names[TERM_INDEX] + nameLen, DTINDEX);
+
+  names[TERM_LOOKUP] = new char[nameLen + strlen(DTLOOKUP) + 1];
+  strncpy(names[TERM_LOOKUP], indexName, nameLen);
+  strcpy(names[TERM_LOOKUP] + nameLen, DTLOOKUP);
+
+  names[TERM_IDS] = new char[nameLen + strlen(TERMIDMAP) + 1];
+  strncpy(names[TERM_IDS], indexName, nameLen);
+  strcpy(names[TERM_IDS] + nameLen, TERMIDMAP);
+
+  names[DOC_IDS] = new char[nameLen + strlen(DOCIDMAP) + 1];
+  strncpy(names[DOC_IDS], indexName, nameLen);
+  strcpy(names[DOC_IDS] + nameLen, DOCIDMAP);
 
   if (!indexLookup())
     return false;
 
   if (!dtLookup())
+    return false;
+
+  if (!dtFileIDs())
     return false;
 
   if (!termIDs())
@@ -43,6 +88,7 @@ bool InvFPIndex::open(const char* indexName){
   if (!docIDs())
     return false;
 
+  cerr << "Load index complete." << endl;
   return true;
 }
 
@@ -58,7 +104,7 @@ int InvFPIndex::term(const char* word){
   return -1;
 }
 
-char* InvFPIndex::term(int termID) {
+const char* InvFPIndex::term(int termID) {
   if ((termID < 0) || (termID > counts[UNIQUE_TERMS]))
     return NULL;
   return terms[termID];
@@ -72,7 +118,7 @@ int InvFPIndex::document(const char* docIDStr){
   return -1;
 }
 
-char* InvFPIndex::document(int docID) { 
+const char* InvFPIndex::document(int docID) { 
   if ((docID < 0) || (docID > counts[DOCS]))
     return NULL;
   return docnames[docID]; 
@@ -82,11 +128,16 @@ int InvFPIndex::termCount(int termID) const{
   // the termCount is equal to the position counts
   // this is calculated by the length of the inverted list -(df*2) for each doc, 
   // there is the docid and the tf
+  if ((termID <0) || (termID > counts[UNIQUE_TERMS])) {
+    cerr << "Error:  Trying to get termCount for invalid termID" << endl;
+    return -1;
+  }
+
   TERM_T t;
   ifstream look;
   look.open(names[DOC_INDEX], ios::in | ios::binary);
   if (!look) {
-    fprintf(stderr, "Could not open indexfile for reading.\n");
+    fprintf(stderr, "Error:  Could not open indexfile for reading.\n");
     return -1;
   }
 
@@ -109,7 +160,7 @@ int InvFPIndex::termCount(int termID) const{
 }
 
 float InvFPIndex::docLengthAvg(){
-  return 0;
+  return aveDocLen;
 }
 
 int InvFPIndex::docCount(int termID) {
@@ -160,7 +211,7 @@ int InvFPIndex::docLengthTotal(int docID){
   int dl;
   DOCID_T id;
   ifstream look;
-  look.open(names[TERM_INDEX], ios::in | ios::binary);
+  look.open(dtfiles[dtlookup[docID].fileid], ios::in | ios::binary);
   if (!look) {
     fprintf(stderr, "Could not open term indexfile for reading.\n");
     return -1;
@@ -179,7 +230,7 @@ int InvFPIndex::docLength(int docID) const{
   int dl;
   DOCID_T id;
   ifstream look;
-  look.open(names[TERM_INDEX], ios::in | ios::binary);
+  look.open(dtfiles[dtlookup[docID].fileid], ios::in | ios::binary);
   if (!look) {
     fprintf(stderr, "Could not open term indexfile for reading.\n");
     return -1;
@@ -254,7 +305,7 @@ TermInfoList* InvFPIndex::termInfoList(int docID){
     return NULL;
 
   ifstream indexin;
-  indexin.open(names[TERM_INDEX], ios::in | ios::binary);
+  indexin.open(dtfiles[dtlookup[docID].fileid], ios::in | ios::binary);
   indexin.seekg(dtlookup[docID].offset, ios::beg);
   TermInfoList* termlist;
   InvFPTermList* tlist = new InvFPTermList();
@@ -271,6 +322,37 @@ TermInfoList* InvFPIndex::termInfoList(int docID){
 /*=======================================================================
  * PRIVATE METHODS 
  =======================================================================*/
+bool InvFPIndex::mainToc(char * fileName) {
+  FILE* in = fopen(fileName, "r");
+  cerr << "Trying to open toc: " << fileName << endl;
+  if (in == NULL) {
+    cerr << "Couldn't open toc file for reading" << endl;
+    return false;
+  }
+
+  char key[128];
+  char val[128];
+  while (!feof(in)) {
+    fscanf(in, "%s %s", key, val);
+    if (strcmp(key, "NUM_DOCS:") == 0) {
+      counts[DOCS] = atoi(val);
+    } else if (strcmp(key, "NUM_TERMS:") == 0) {
+      counts[TOTAL_TERMS] = atoi(val);
+    } else if (strcmp(key, "NUM_UNIQUE_TERMS:") == 0) {
+      counts[UNIQUE_TERMS] = atoi(val);
+    } else if (strcmp(key, "NUM_DTFILES:") == 0) {
+      counts[DT_FILES] = atoi(val);
+    } else if (strcmp(key, "NUM_INVFILES:") == 0) {
+      counts[INV_FILES] = atoi(val);
+    }
+
+  }
+
+  aveDocLen = counts[TOTAL_TERMS] / (float) counts[DOCS];
+
+  return true;
+}
+
 bool InvFPIndex::indexLookup() {
   FILE* in = fopen(names[DOC_LOOKUP], "rb");
   cerr << "Trying to open invlist lookup: " << names[DOC_LOOKUP] << endl;
@@ -316,8 +398,40 @@ bool InvFPIndex::dtLookup() {
   return true;
 }
 
+bool InvFPIndex::dtFileIDs() {
+  FILE* in = fopen(names[TERM_INDEX], "rb");
+  cerr << "Trying to open term index filenames: " << names[TERM_INDEX] << endl;
+  if (in == NULL) {
+    fprintf(stderr, "Error opening term index filenames file\n");
+    return false;
+  }
+
+  dtfiles = new char*[counts[DT_FILES]];
+
+  int index;
+  int len;
+  char* str;
+
+  while (!feof(in)) {
+    if (fscanf(in, "%d %d", &index, &len) != 2) 
+        continue;
+
+    str = (char*) malloc(sizeof(char) * (len+1));
+    if (fscanf(in, "%s", str) != 1) {
+       free(str);
+       continue;
+    }
+
+    dtfiles[index] = str;
+  }
+
+  fclose(in);
+  return true;
+}
+
 bool InvFPIndex::termIDs() {
   FILE* in = fopen(names[TERM_IDS], "rb");
+  cerr << "Trying to open term ids file: " << names[TERM_IDS] << endl;
   if (in == NULL) {
     fprintf(stderr, "Error opening termfile\n");
     return false;
@@ -353,6 +467,7 @@ bool InvFPIndex::termIDs() {
 
 bool InvFPIndex::docIDs() {
   FILE* in = fopen(names[DOC_IDS], "rb");
+  cerr << "Trying to open doc ids file: " << names[DOC_IDS] << endl;
   if (in == NULL) {
     fprintf(stderr, "Error opening docfile\n");
     return false;
