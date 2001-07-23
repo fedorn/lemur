b9441e5613fd87d94b85b3e433ee89a7e458672f
czhai <czhai>
2001-07-23 05:20:08 +0000

    minor correction

---
+++ index/include/IndexManager.hpp
@@ -0,0 +1,15 @@
+#ifndef _INDEXMANAGER_HPP
+#define _INDEXMANAGER_HPP
+
+#include "BasicIndex.hpp"
+#include "InvFPIndex.hpp"
+
+/// A group of index management functions
+class IndexManager {
+  /// An utility function to open an index, automatically recognizing the indexer based on file extension
+
+  static Index *openIndex(const char *indexTOCFile);
+};
+
+
+#endif /* _INDEXMANAGER_HPP */
