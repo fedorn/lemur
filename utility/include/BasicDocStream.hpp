3d7ff6b7d0a1ac183738f4052f725f70a549e2f5
czhai <czhai>
2001-08-01 01:39:38 +0000

    added copyright

--- utility/include/BasicDocStream.hpp
+++ utility/include/BasicDocStream.hpp
@@ -1,3 +1,18 @@
+ /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * 
+ * The Lemur toolkit for language modeling and information retrieval.
+ * Copyright (c) 2001 Carnegie Mellon University.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted for research or educational purposes,
+ * provided that this copyright notice is maintained and note is made
+ * of any changes to the source code.
+ * 
+ * This is a research system.  The code is distributed on an "as is" basis,
+ * without any warranty, express or implied. 
+ * 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #ifndef _BASICFILESTREAM_HPP
 #define _BASICFILESTREAM_HPP
 
