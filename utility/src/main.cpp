4dfd905c111d9b7e9e677b90e778883b08afe14a
pto <pto>
2001-11-01 19:18:39 +0000

    Allowed for programs w/ more than one arguement.

--- utility/src/main.cpp
+++ utility/src/main.cpp
@@ -19,11 +19,13 @@
 
 int main(int argc, char *argv[])
 {
-  if (argc==2 && strcmp(argv[1], "-help") &&
+  if (argc>=2 && strcmp(argv[1], "-help") &&
       strcmp(argv[1], "--help") &&
       strcmp(argv[1], "-h") ) {  // recognize parameter file as argument
     ParamPushFile(argv[1]);
-  } else if (argc==2) { // handle "-help", display parameters 
+  } else {
+    
+    // handle "-help", display parameters 
     GetAppParam(); // called only  to "register" parameters
     cerr << "Parameters for " <<  argv[0] << endl;
     ParamDisplay();
