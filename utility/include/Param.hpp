4fee352bb1ea0f27476feed8cc51a2b8d4cc0e9c
tnt <tnt>
2001-11-30 18:06:27 +0000

    windows happy

--- utility/include/Param.hpp
+++ utility/include/Param.hpp
@@ -1,14 +1,17 @@
-/*==========================================================================
- * Copyright (c) 2001 Carnegie Mellon University.  All Rights Reserved.
+ /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+ * 
+ * The Lemur toolkit for language modeling and information retrieval.
+ * Copyright (c) 2001 Carnegie Mellon University.  All rights reserved.
  *
- * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
- * is subject to the terms of the software license set forth in the LICENSE
- * file included with this software, and also available at
- * http://www.cs.cmu.edu/~lemur/license.html
- *
- *==========================================================================
-*/
-
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted for research or educational purposes,
+ * provided that this copyright notice is maintained and note is made
+ * of any changes to the source code.
+ * 
+ * This is a research system.  The code is distributed on an "as is" basis,
+ * without any warranty, express or implied. 
+ * 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #ifndef _ParamH_
 #define _ParamH_
@@ -32,14 +35,6 @@ int    ParamGetInt(const String &s, int def);
 int    ParamGetBit(const String &s, int def);
 double ParamGetDouble(const String &s, double def);
 float  ParamGetFloat(const String &s, float def);
-
-//  Put routines:
-/////////////////////////////////////
-void   ParamSetRestartFile(const String &s);
-void   ParamPut(const String &s, const double &value);
-void   ParamPut(const String &s, const float  &value);
-void   ParamPut(const String &s, const int    &value);
-void   ParamPut(const String &s, const String &value);
 
 //  Checkpointing:
 /////////////////////////////////////
