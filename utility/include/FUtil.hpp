6ef6a9814fdfecb02f4b89f850fbdadb219f74c7
tnt <tnt>
2002-03-28 20:28:30 +0000

    fileExist utility

---
+++ utility/include/FUtil.hpp
@@ -0,0 +1,17 @@
+/*==========================================================================
+ * Copyright (c) 2001 Carnegie Mellon University.  All Rights Reserved.
+ *
+ * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
+ * is subject to the terms of the software license set forth in the LICENSE
+ * file included with this software, and also available at
+ * http://www.cs.cmu.edu/~lemur/license.html
+ *
+ *==========================================================================
+*/
+
+#ifndef _FUTIL_HPP_
+#define _FUTIL_HPP_
+
+bool fileExist(char *name);
+
+#endif
---
+++ utility/src/FUtil.cpp
@@ -0,0 +1,25 @@
+/*==========================================================================
+ * Copyright (c) 2001 Carnegie Mellon University.  All Rights Reserved.
+ *
+ * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
+ * is subject to the terms of the software license set forth in the LICENSE
+ * file included with this software, and also available at
+ * http://www.cs.cmu.edu/~lemur/license.html
+ *
+ *==========================================================================
+*/
+
+#include "FUtil.hpp"
+#include "common_headers.hpp"
+
+bool fileExist(char* name)
+{
+  ifstream ifs;
+  ifs.open(name);
+  if (!ifs.fail()) {
+    ifs.close();
+    return true;
+  } 
+  return  false;
+}
+
