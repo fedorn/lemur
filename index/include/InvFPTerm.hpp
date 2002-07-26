64011114437ed0c0741e4a201fbc9c11bfa8064b
tnt <tnt>
2002-07-26 17:57:32 +0000

    spelling needs to return const char*

--- index/include/InvFPTerm.hpp
+++ index/include/InvFPTerm.hpp
@@ -27,7 +27,7 @@ public:
   InvFPTerm() { freq=1;};
   ~InvFPTerm() {};
 
-  char* spelling() { return word; };
+  const char* spelling() { return word; };
   void spelling(TERM_T term) { word = term; };
   int strLength() { return strlen; };
   void strLength(int len) { strlen = len; };
