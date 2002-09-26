d7572104d2cd3a00399c1053a32c80c1722fd1d1
tnt <tnt>
2002-09-26 18:33:47 +0000

    single regression merge

---
+++ distrib/src/SingleRegrMergeMethod.cpp
@@ -0,0 +1,122 @@
+/*==========================================================================
+ * Copyright (c) 2002 Carnegie Mellon University.  All Rights Reserved.
+ *
+ * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
+ * is subject to the terms of the software license set forth in the LICENSE
+ * file included with this software, and also available at
+ * http://www.cs.cmu.edu/~lemur/license.html
+ *
+ *==========================================================================
+*/
+
+/*
+ * NAME DATE - COMMENTS
+ * lsi 09/2002 - created
+ *
+ *========================================================================*/
+
+#include "SingleRegrMergeMethod.hpp"
+#include <map>
+
+typedef map<char *, double, ltstr> docsidmap;
+
+
+SingleRegrMergeMethod::SingleRegrMergeMethod(double constA, double constB) {
+  parama = constA;
+  paramb = constB;
+}
+
+SingleRegrMergeMethod::~SingleRegrMergeMethod() {
+}
+
+/// Calculate the final comparable document score
+double SingleRegrMergeMethod::score(double dbscore, double docscore) {
+  return parama * docscore + paramb * dbscore * docscore;
+}
+
+/// calculate the single regression model for all the databases
+/// indexset are the database scores for selected databases
+/// centralsocres are the central documents scores retrieved by centralized sampling database
+/// scoresset are the distributed documents scores retrieved by individual databases
+void SingleRegrMergeMethod::calcRegrParams(IndexedRealVector &indexset, DocScoreVector* centralscores, DocScoreVector** scoresset) {
+  DocScoreVector* docscores;
+  DocScoreVector::iterator j;
+  docsidmap centerScoreHash;
+  double indexscore;
+  int maxArraySize=0;
+
+
+  for (int i=0;i<indexset.size();i++) {
+    docscores = scoresset[i];
+    maxArraySize+=docscores->size();
+  }
+
+  /// the following arrays are created to calculate the linear regression parameters.
+  /// here we wants to figure out the parameters of D''=a*D'+b*C'*D';
+  /// Yi=a*X1i+b*X2i
+  double yVals[maxArraySize];
+  double x1Vals[maxArraySize];
+  double dbVals[maxArraySize];
+  double x2Vals[maxArraySize];
+  double sum_x1_x1=0;
+  double sum_x2_x2=0;
+  double sum_x1_x2=0;
+  double sum_y_x1=0;
+  double sum_y_x2=0;
+  double aVal=0;//a Parameter
+  double bVal=0;//b Parameter
+
+  for (j=centralscores->begin(); j!= centralscores->end();j++) {
+    if ((*j).val!=0){
+      //put all the centralized scores into a hash map
+      centerScoreHash[strdup((*j).id)]=(*j).val;
+    }
+  }
+ 
+  int overlapNum=0;//the total number of overlap documents for all databases.
+  int lessDbNum=0; //the number of databases that has less overlap documents than the threshold.
+  int dbCount=indexset.size();
+  for (int i=0;i<indexset.size();i++) {
+    int dbOverlapNum=0; //the number of overlap documents for this specific database
+    docscores = scoresset[i];
+    indexscore = indexset[i].val;
+    for (j=docscores->begin(); j!= docscores->end();j++) {
+      char* docId=(char *)(*j).id;
+      if  ((centerScoreHash[docId]!=0) && ((*j).val!=0) && (dbOverlapNum<GETTOPDOCSNUM)){
+	double y= centerScoreHash[docId];
+	double x1=(*j).val;
+	double x2=(*j).val*indexscore;
+	yVals[overlapNum]=y;
+	x1Vals[overlapNum]=x1;
+	dbVals[overlapNum]=indexscore;
+	x2Vals[overlapNum]=x2;
+	sum_x1_x1+=x1*x1;
+	sum_x2_x2+=x2*x2;
+	sum_x1_x2+=x1*x2;
+	sum_y_x1+=y*x1;
+	sum_y_x2+=y*x2;
+	dbOverlapNum++;
+	overlapNum++;
+      }
+    }
+    if (dbOverlapNum<MINOVERLAPDOCNUM){
+      lessDbNum++;
+    }
+  }
+
+  if (((double)lessDbNum/(double)dbCount)<=MINLESSDBPERCEN){
+    //we have enough databases that have enough overlap documents
+    //start to calculate the parameters
+    bVal=sum_y_x2*sum_x1_x1-sum_y_x1*sum_x1_x2;
+    bVal/=(sum_x1_x1*sum_x2_x2-sum_x1_x2*sum_x1_x2);
+    aVal=sum_y_x1-bVal*sum_x1_x2;
+    aVal/=sum_x1_x1;
+    parama=aVal;
+    paramb=bVal;
+  }else{
+    //we do not have enough databases that have enough overlap documents, back off to the CORI merging algorithm
+    parama=0.71428;
+    paramb=0.5714;
+  }
+}
+
