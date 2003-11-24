a8653f3abaf026e0a791df863f9501cf1263f5be
tnt <tnt>
2003-11-24 20:28:44 +0000

    new implementation to push properties into index

---
+++ index/include/PropIndexTH.hpp
@@ -0,0 +1,71 @@
+/*==========================================================================
+ *
+ *  Original source copyright (c) 2003, Carnegie Mellon University.
+ *  See copyright.cmu for details.
+ *
+ *==========================================================================
+*/
+
+#include "TextHandler.hpp"
+#include "InvFPPushIndex.hpp"
+
+
+#ifndef _PROPINDEXTH_HPP
+#define _PROPINDEXTH_HPP
+
+#define MAX_WORD_LENGTH 25
+
+/**
+   PropIndexTH builds a positional indexer. At this time only InvFPIndex
+   is supported. This class is a destination TextHandler. It expects the 
+   position of the token to be given (not counted) and will index
+   certain properties if passed in the PropertyList. PropertyList *must*
+   have one property called "position" with the token position. 
+   Any and ALL other properties of type STRING will be indexed at the 
+   same position as the token, but added after the token. This is usually 
+   the last object in TextHandler chain. If a property should NOT be indexed, 
+   remove it before it gets here.
+*/
+
+class PropIndexTH : public TextHandler {
+
+public:
+  // want to subclass this class for incremental.
+  PropIndexTH() { }
+  
+  /// Create a PropIndexTH with index name filename 
+  /// (minus the .ifp extension) and specified buffer size.
+  PropIndexTH(char * filename, int bufferSize, bool countStopWds = false, int ind = 1);
+  ~PropIndexTH();
+
+  /// Pushes the doc into the InvFPPushIndex.
+  char * handleDoc(char * docno);
+  /// Pushes the word into the InvFPPushIndex.
+  char * handleWord(char * word, char* original, PropertyList* list);
+
+  /// passes mgrID onto index
+  void setDocManager(const char* mgrID);
+  // want to subclass this class for incremental.
+protected:
+  /// Ends a document in the collection
+  void endDoc();
+
+  /// The indexer.
+  PushIndex * index;
+  /// Document properties object to be reused over repeated calls.
+  DocumentProps * dp;
+  /// Term object to be reused over repeated calls.
+  InvFPTerm * term;
+  /// Keeps track of document length.
+  int docLength;
+
+  /// Indicates whether or not to count stop words in doc length
+  bool countStopWds;
+
+  /// Indicates whether object is before first doc
+  /// true = yes, false = foundDoc has been called once.
+  bool first;
+};
+
+#endif
+
---
+++ index/src/PropIndexTH.cpp
@@ -0,0 +1,148 @@
+/*==========================================================================
+ *
+ *  Original source copyright (c) 2003, Carnegie Mellon University.
+ *  See copyright.cmu for details.
+ *
+ *==========================================================================
+*/
+#include "PropIndexTH.hpp"
+
+PropIndexTH::PropIndexTH(char * filename, int bufferSize,
+			 bool countStopWords, int ind) {
+  // create index and helper objects  
+  if (ind == 1)
+    index = new InvFPPushIndex(filename, bufferSize);
+  // add support for future positional indexes here
+  else 
+    throw Exception("PropIndexTH", "Unknown index type");
+
+  dp = NULL;
+  term = new InvFPTerm();
+  countStopWds = countStopWords;
+  docLength = 0;
+  // set state that is on first doc
+  first = true;
+}
+
+PropIndexTH::~PropIndexTH() {
+  // end the doc and close the collection
+  if (!first) endDoc(); // if we haven't done any yet, don't bother.
+  index->endCollection(NULL);
+  delete dp;
+  delete term;
+  delete index;
+}
+
+void PropIndexTH::endDoc() {
+  dp->length(docLength);
+  index->endDoc(dp);
+  dp->stringID(NULL);
+}
+
+char* PropIndexTH::handleDoc(char* docno) {
+  // finish the old doc  
+  if (!first) {
+    endDoc();
+  } else {
+    dp = new DocumentProps();
+    first = false;
+  }
+
+  // begin the new document
+  dp->length(0);  
+  docLength = 0;
+  dp->stringID(docno);
+  index->beginDoc(dp);
+  return docno;
+}
+
+char* PropIndexTH::handleWord(char* word, char* original, PropertyList *list) {
+  Property* prop = NULL;
+  int position = -1;
+  char* tag = NULL;
+  if (word) {
+    // ignore words longer than max characters
+    int len = strlen(word);
+    if (len <= MAX_WORD_LENGTH) {
+      // get position first
+      prop = list->getProperty("position");
+      if (!prop)
+	throw Exception("PropIndexTH","missing \"position\" property in property list");
+      position = *((int*) prop->getValue());
+      term->position(position);
+      if (len > 0) {
+	// index the word 
+	term->strLength(len);
+	term->spelling(word);
+	index->addTerm(*term);
+	docLength++;
+      }
+
+      // index all STRING properties
+      list->startIteration();
+      while (list->hasMore()) {
+	prop = list->nextEntry();
+	if (prop->getType() == Property::STRING) {
+	  tag = (char*) prop->getValue();
+	  if (tag) {
+	    term->strLength(strlen(tag));
+	    if (term->strLength() > 0) {
+	      term->spelling(tag);
+	      index->addTerm(*term);
+	    }
+	  } // if tag 
+	}// if prop is string
+      } // while more props
+    }
+  } else {
+    if (countStopWds) docLength++;
+    // when the word is null, it usually means it was a stopword
+    // sometimes stop words have named entity tags, in this case we want to index it
+    // check to see if it has a named entity tag
+    prop = list->getProperty("NE");
+    if ((prop) && (prop->getLength() > 1)) {
+      // index the term
+      // still ignore words longer than max characters
+      int len = strlen(original);
+      if (len <= MAX_WORD_LENGTH) {
+	//	tag = (char*) prop->getValue();
+	// get position first
+	prop = list->getProperty("position");
+	if (!prop)
+	  throw Exception("PropIndexTH","missing \"position\" property in property list");
+	position = *((int*) prop->getValue());
+	term->position(position);
+	if (len > 0) {
+	  // index the word 
+	  term->strLength(len);
+	  term->spelling(original);
+	  index->addTerm(*term);
+	}
+
+	// index the NE tag
+	prop = list->getProperty("NE");
+	tag = (char*)prop->getValue();
+	term->strLength(strlen(tag));
+	term->spelling(tag);
+	index->addTerm(*term);
+
+	// index the B_NE tag if any
+	prop = list->getProperty("B_NE");
+	if (prop) {
+	  tag = (char*)prop->getValue();
+	  term->strLength(strlen(tag));
+	  if (term->strLength() > 0) {
+	    term->spelling(tag);
+	    index->addTerm(*term);
+	  }
+	} // B_NE
+      }     
+    } // if NE
+  } // else word is NULL
+  return word;
+}
+
+
+void PropIndexTH::setDocManager(const char* mgrID) {
+  index->setDocManager(mgrID);
+}
