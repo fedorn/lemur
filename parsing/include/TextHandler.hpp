dc7fba819e5107f5e861ccb46878af473b816f35
tnt <tnt>
2003-11-24 20:19:01 +0000

    udpated API for Properties, getTH, virtual destructor

--- parsing/include/TextHandler.hpp
+++ parsing/include/TextHandler.hpp
@@ -9,6 +9,9 @@
  *==========================================================================
 */
 
+#include <cstring>
+#include "common_headers.hpp"
+
 #ifndef NULL
 #define NULL 0
 #endif
@@ -16,6 +19,10 @@
 #ifndef _TEXTHANDLER_HPP
 #define _TEXTHANDLER_HPP
 
+#include "PropertyList.hpp"
+
+
+#define MAXWORDSIZE 1024
 
 ///  This class serves as an interface for classes working with the parsers.
 ///  The setTextHandler function allows chaining of TextHandlers, so that 
@@ -33,7 +40,15 @@
 ///  for all functions.  When their foundDoc or foundWord is called, they
 ///  will possibly manipulate the data, then forward the info via
 ///  calling the foundDoc/foundWord function of their TextHandler.
+///  The original should be preserved and passed on as is. 
+///  Properties can be associated with token using the PropertyList.
 
+
+///  TextHandlers have their own internal buffer for modification of
+///  the string.  The foundWord function copies the word into the buffer
+///  then calls handleWord with the copy.  The handleWord function may
+///  then modify the string and return the pointer to the string. 
+///  This process is also done for foundDoc/handleDoc.
 
 //  Might make more sense as TextSource and TextDestination with
 //  functions in the middle of the chain inheriting from both.
@@ -42,45 +57,139 @@
 class TextHandler {
 
 public:
+  enum TokenType {BEGINDOC = 1, ENDDOC = 2, WORD = 3, 
+		  BEGINTAG = 4, ENDTAG = 5, SYMBOL = 6};
+
   TextHandler() {
     textHandler = NULL;
+    buffer[MAXWORDSIZE-1] = '\0';
   }
-
-  /// Found a document with document number
-  virtual void foundDoc(char * docno) {
-    docno = handleDoc(docno);
-    if (textHandler != NULL) 
-      textHandler->foundDoc(docno);
-  }
-  /// Found a word
-  virtual void foundWord(char * word) {
-    word = handleWord(word);
-    if (textHandler != NULL)
-      textHandler->foundWord(word);
-  }
-
-  /// Found a word
-  virtual void foundSymbol(char * sym) {
-    sym = handleSymbol(sym);
-    if (textHandler != NULL)
-      textHandler->foundSymbol(sym);
-  }
-
+  virtual ~TextHandler() {}
+  
   /// Set the TextHandler that this TextHandler will pass information on to.
   virtual void setTextHandler(TextHandler * th) {
     textHandler = th;
   }
+  /// Set the TextHandler that this TextHandler will pass information on to.
+  virtual TextHandler * getTextHandler() {
+    return textHandler;
+  }
 
+  virtual void foundToken(int type, 
+			  char * token = NULL, 
+			  char * orig = NULL,
+			  PropertyList * properties = NULL) {
+    char * t = NULL;
+
+    if (token != NULL) {
+      strncpy(buffer, token, MAXWORDSIZE - 1);
+      t = buffer;
+    } 
+
+    switch (type) {
+
+    case BEGINDOC:
+      t = handleBeginDoc(t, orig, properties);
+      break;
+    case ENDDOC:
+      t = handleEndDoc(t, orig, properties);
+      break;
+    case WORD:
+      t = handleWord(t, orig, properties);
+      break;
+    case BEGINTAG:
+      t = handleBeginTag(t, orig, properties);
+      break;
+    case ENDTAG:
+      t = handleEndTag(t, orig, properties);
+      break;            
+    case SYMBOL:
+      t = handleSymbol(t, orig, properties);
+      break;            
+    }
+
+    if (textHandler != NULL) {
+      textHandler->foundToken(type, t, orig, properties);
+    }
+  }
+
+  /// Handle a doc begin - default implementation calls
+  /// handleDoc for backwords compat
+  virtual char * handleBeginDoc(char * docno, char * original,
+				PropertyList * list) {
+    return handleDoc(docno);
+  }
+  /// Handle a doc end - default implementation calls
+  /// old handleEndDoc for backwords compat
+  virtual char * handleEndDoc(char * token, char * original,
+			      PropertyList * list) {
+    handleEndDoc();
+    return token;
+  }
+  /// Handle a word - default implementation calls
+  /// old handleWord for backwords compat
+  virtual char * handleWord(char * word, char * original,
+			    PropertyList * list) {
+    return handleWord(word);
+  }
+  /// Handle a begin tag
+  virtual char * handleBeginTag(char * tag, char * original,
+				PropertyList * list) {
+    return tag;
+  }
+  /// Handle an end tag 
+  virtual char * handleEndTag(char * tag, char * original,
+			      PropertyList * list) {
+    return tag;
+  }
+
+  /// Handle a symbol - default implementation calls
+  /// old handleSymbol for backwords compat
+  virtual char * handleSymbol(char * symbol, char * original,
+			      PropertyList * list) {
+    return handleSymbol(symbol);
+  }
+
+
+
+  // For backwards compatability
+  /// Found a document with document number
+  virtual void foundDoc(char * docno) {
+    foundToken(BEGINDOC, docno, docno);
+  }
+  virtual void foundDoc(char * docno, char * original) {
+    foundToken(BEGINDOC, docno, original);
+  }
+  /// Found a word
+  virtual void foundWord(char * word) {
+    foundToken(WORD, word, word);
+  }
+  virtual void foundWord(char * word, char * original) {
+    foundToken(WORD, word, original);
+  }
+  /// Found end of doc
+  virtual void foundEndDoc() {
+    foundToken(ENDDOC);
+  }
+  /// Found a word
+  virtual void foundSymbol(char * sym) {
+    foundToken(SYMBOL, sym, sym);
+  }  
+  // Kept for backwords compatability
   /// Handle a doc
   virtual char * handleDoc(char * docno) { return docno; }
   /// Handle a word, possibly transforming it
   virtual char * handleWord(char * word) { return word; }
+  /// Handle the end of the doc
+  virtual void handleEndDoc() { }
   /// Handle a word, possibly transforming it
   virtual char * handleSymbol(char * sym) { return sym; }
 
 protected:
   /// The next textHandler in the chain
   TextHandler * textHandler;
+
+  char buffer[MAXWORDSIZE];
 };
 
 #endif
