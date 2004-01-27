fd27fdc5044caaf86a9f07ca38bf48816d10b7d4
tnt <tnt>
2004-01-27 19:40:30 +0000

    add MultiRegrMerge

--- app/src/DistRetEval.cpp
+++ app/src/DistRetEval.cpp
@@ -12,13 +12,13 @@
 
 /** author: tnt, lsi */
 
-/** 
+/*!
     This is a sample application that does distributed retrieval, using a
     resource selection index and individual indexes. resource selection is done
     using the CORI_CS  (the only resource selection implemented thus far).
-    results merging uses either CORIMergeMethod (The retrieval method of each
-    individual databases should be INQUERY) or SingleRegrMergeMethod (The
-    retrieval method of each individual databases should be INQUERY).
+    results merging uses either CORIMergeMethod, SingleRegrMergeMethod, or MultiRegrMergeMethod.
+    (If using CORIMerge, should use INQUERY for the retrieval method of each 
+    individual database.)
 
     Parameters should be set as follows:
     index = the collection selection database
@@ -28,7 +28,7 @@
     resultCount = maximum number of results to output for each query (default to 1000)
     textQuery = file of text queries in docstream format
     cutoff = maximum number of databases to search (default to 10)
-    "dbids" = "db's param file" - required for each database in the collection selection index.  key should be the dbcharid and value should be name of file that has parameters for that database: 
+    "dbids" = "db's param file" - required for each database in the collection selection index.  key should be the db's string id as it was named/indexed in the collection selection index,  and value should be name of file that has parameters for that database: 
             index = the individual database
 	    retModel = the retrieval model to use
 	    mergeMethod = the merging method to use
@@ -40,13 +40,13 @@
     selection method.
 
     mergeMethod  resource merging method (0 for CORI results merging
-    method, 1 for single regression results merging method)
+    method, 1 for single regression results merging method, 2 for multiple regression merge)
 
     Merginge Method-specific parameters:
 
-         For CORI mergine Method: None
+         For CORI merging Method: None
     
-	 For  Single regression merging method:
+	 For  Single and Multiple regression merging methods:
     
 	 1. csDbDataBaseIndex the centralized sampling database index
 
@@ -55,6 +55,7 @@
 	 
          3. DOCTF_baseline The TFbaseline parameter in the INQUERY retrieval method for the centralized sampling database.
 
+
 **/
 
 #include "CORIRetMethod.hpp"
@@ -62,6 +63,7 @@
 #include "DistMergeMethod.hpp"
 #include "CORIMergeMethod.hpp"
 #include "SingleRegrMergeMethod.hpp"
+#include "MultiRegrMergeMethod.hpp"
 #include "Param.hpp"
 #include "RetParamManager.hpp"
 #include "IndexManager.hpp"
@@ -71,11 +73,11 @@ namespace LocalParameter {
   // the index cutoff for how many indexes to search
   static int cutoff;
   static string ranksfile;
-  static char* textQuerySet2;
+  //  static char* textQuerySet2;
   static void get() {
     cutoff = ParamGetInt("cutoff", 10); // default is 10
     ranksfile = ParamGetString("ranksFile", ""); // write out ranking scores
-    textQuerySet2=strdup(ParamGetString("textQuerySet2",""));
+    //    textQuerySet2=strdup(ParamGetString("textQuerySet2",""));
   }
 };
 
@@ -104,10 +106,27 @@ int AppMain(int argc, char *argv[]) {
 
 
   if ( DistMergeMethodParameter::mergeMethod==SINGLETYPEREGR_MERGE){
-    //use some regression method
+    //use single type regression method
     try {
       SingleRegrMergeMethodParameter::get();
       csdbindex = IndexManager::openIndex(SingleRegrMergeMethodParameter::csDbDataBaseIndex);
+      int doccount = csdbindex->docCount();
+      accumCsDb=new ArrayAccumulator (doccount);
+      rankingsCsDb=new IndexedRealVector(doccount);
+      modelCsDb=new   CORIRetMethod (*csdbindex,*accumCsDb,"USE_INDEX_COUNTS",0);
+
+    }
+    catch (Exception &ex) {
+      ex.writeMessage(cerr);
+      throw Exception("DistRetEval", "Can't open the centralized database index, check parameter index");
+    }
+  }
+
+  if ( DistMergeMethodParameter::mergeMethod==MULTITYPEREGR_MERGE){
+    //use multiple type regression method
+    try {
+      MultiRegrMergeMethodParameter::get();
+      csdbindex = IndexManager::openIndex(MultiRegrMergeMethodParameter::csDbDataBaseIndex);
       int doccount = csdbindex->docCount();
       accumCsDb=new ArrayAccumulator (doccount);
       rankingsCsDb=new IndexedRealVector(doccount);
@@ -133,25 +152,17 @@ int AppMain(int argc, char *argv[]) {
 
   int i;
  
-  //creat the collecion selection retireval model
+  //create the collection selection retrieval model
   int doccount = csindex->docCount();
   ArrayAccumulator accum(csindex->docCount());
   IndexedRealVector rankings(doccount);
-  //creat the collecion selection retireval model
   CORIRetMethod model(*csindex, accum, CORIParameter::collectionCounts,1);
   DocScoreVector results;
 
-
   //Notice assume that collectionCounts i USE_INDEX_COUNT
-
-
   DistSearchMethod search(csindex,RetMethodManager::INQUERY);
   DocScoreVector** scoreset = new DocScoreVector*[LocalParameter::cutoff];
   DocScoreVector resultsCsDb;
-  CORIMergeMethod merger;
-  SingleRegrMergeMethod singleRegrMerger;
-
-
 
   cout<<"Write Results File to "<<RetrievalParameter::resultFile<<endl;
   ofstream resfile(RetrievalParameter::resultFile);
@@ -170,9 +181,9 @@ int AppMain(int argc, char *argv[]) {
     q = new TextQuery(*d);
     cout << "query : " << q->id() << endl;
 
-    //to calculate the centralized scores;
- 
-    if (DistMergeMethodParameter::mergeMethod==SINGLETYPEREGR_MERGE){
+    //to calculate the centralized scores; 
+    if (DistMergeMethodParameter::mergeMethod==SINGLETYPEREGR_MERGE ||
+	DistMergeMethodParameter::mergeMethod==MULTITYPEREGR_MERGE){
       QueryRep *qrCs = modelCsDb->computeQueryRep(*q);
       resultsCsDb.clear();
       modelCsDb->scoreCollection(*qrCs, *rankingsCsDb);    
@@ -200,7 +211,6 @@ int AppMain(int argc, char *argv[]) {
     }
 
     // we want to search only so many databases
-
    if(rankings.size()>LocalParameter::cutoff)
      rankings.assign(rankings.begin(), 
 		     rankings.begin()+LocalParameter::cutoff);
@@ -208,13 +218,19 @@ int AppMain(int argc, char *argv[]) {
     search.setReturnCount(RetrievalParameter::resultCount);
     // search those databases and put the scores into scoreset
     search.scoreIndexSet(*q, rankings, scoreset);
-    // now merge the scores
 
+    // now merge the scores
     if (DistMergeMethodParameter::mergeMethod==CORI_MERGE){
+      CORIMergeMethod merger;
       merger.mergeScoreSet(rankings, scoreset, results);
     }else if  (DistMergeMethodParameter::mergeMethod==SINGLETYPEREGR_MERGE){
+      SingleRegrMergeMethod singleRegrMerger;
       singleRegrMerger.calcRegrParams(rankings,&resultsCsDb,scoreset);
       singleRegrMerger.mergeScoreSet(rankings, scoreset, results);
+    }else if  (DistMergeMethodParameter::mergeMethod==MULTITYPEREGR_MERGE){
+      MultiRegrMergeMethod multiRegrMerger;
+      multiRegrMerger.calcRegrParams(rankings,&resultsCsDb,scoreset);
+      multiRegrMerger.mergeScoreSet(rankings, scoreset, results);
     }
 
     for (i=0;i<RetrievalParameter::resultCount && i<results.size();i++) 
@@ -238,7 +254,8 @@ int AppMain(int argc, char *argv[]) {
   delete qryStream;
   delete[]scoreset;
 
-  if ( DistMergeMethodParameter::mergeMethod==SINGLETYPEREGR_MERGE){
+  if ( DistMergeMethodParameter::mergeMethod==SINGLETYPEREGR_MERGE ||
+       DistMergeMethodParameter::mergeMethod==MULTITYPEREGR_MERGE){
     delete accumCsDb;
     delete rankingsCsDb;
     delete modelCsDb;
