a7f1a276a9c74f9f6f69e8e7618395f93764b98a
tnt <tnt>
2004-06-28 18:43:40 +0000

    Query of terms iteration API

---
+++ retrieval/include/TermQuery.hpp
@@ -0,0 +1,28 @@
+/*==========================================================================
+ * Copyright (c) 2004 Carnegie Mellon University.  All Rights Reserved.
+ *
+ * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
+ * is subject to the terms of the software license set forth in the LICENSE
+ * file included with this software, and also available at
+ * http://www.cs.cmu.edu/~lemur/license.html
+ *
+ *==========================================================================
+*/
+
+#ifndef _TERMQUERY_HPP
+#define _TERMQUERY_HPP
+
+#include "Term.hpp"
+#include "RetrievalMethod.hpp" // for Query
+
+/// Abstract interface for a query containing a sequence of Terms
+class TermQuery : public Query {
+public:
+  virtual ~TermQuery() {}
+
+  virtual void startTermIteration() const = 0;
+  virtual bool hasMore() const = 0;
+  virtual const Term *nextTerm() const = 0;
+};
+
+#endif
