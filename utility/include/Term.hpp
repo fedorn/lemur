83ab85a1f62e62067deeda6f1e1c9fe80f2457bf
tnt <tnt>
2004-06-28 16:47:58 +0000

    implement Term object without TermInfo methods

--- utility/include/Term.hpp
+++ utility/include/Term.hpp
@@ -16,17 +16,30 @@
 
 /// Basic term class
 /*! 
-  This abstract class represents a term in a document. 
-  It is currently empty and mainly serves as a high-level concept.
+  This class represents a term in a document. 
 */
 
 class Term {
 public:
-
-  virtual ~Term() {};
-  virtual const char* spelling() const { throw Exception("Term", "spelling() not supported by subclass");}
-  virtual int count() const { throw Exception("Term", "count() not supported by subclass");}
-  virtual int id() const { throw Exception("Term", "id() not supported by subclass");}
+  Term() { term=NULL; termptr=NULL;}
+  virtual ~Term() { free(term); }
+  /// return the spelling of the term (read-only)
+  virtual const char* spelling() const { if (term) return term; return termptr; }
+  /// set the spelling of the term by pointing to the given parameter
+  /// value at str must stay valid for this Term object to stay valid
+  virtual void spelling(const char* str) { free(term); termptr = str; }
+  /// set the spelling of the term by pointing to the given parameter
+  /// value at str must stay valid for this Term object to stay valid
+  virtual void spelling(const string& str) { free(term); termptr = str.c_str(); }
+  /// set the spelling of the term by making a copy
+  virtual void copyspelling(const char* str) { reset(); term=strdup(str); }
+  /// free the copy of the spelling of the term
+  virtual void freespelling() { free(term); }
+  /// reset this term representation
+  virtual void reset() { free(term); termptr=NULL; }
+protected:
+  char* term;
+  const char* termptr;			
 };
 
 
