95db8f3d4d8015ca6a56169b954108bd465f658f
tnt <tnt>
2004-06-29 05:15:37 +0000

    static methods to run query

--- retrieval/include/RetMethodManager.hpp
+++ retrieval/include/RetMethodManager.hpp
@@ -13,16 +13,40 @@
 #define _RETMETHODMANAGER_HPP
 
 #include "RetParamManager.hpp"
+#include "TextHandlerManager.hpp"
+#include "InQueryOpParser.hpp"
+#include "StringQuery.hpp"
 
 class RetMethodManager {
 public:
-  enum RetModel {UNKNOWN=-1, TFIDF=0, OKAPI=1, KL=2, INQUERY=3,CORI_CS=4,COS=5};
+  enum RetModel {UNKNOWN=-1, TFIDF=0, OKAPI=1, KL=2, INQUERY=3,CORI_CS=4,COS=5, INQ_STRUCT};
 
   /// use specified model or check in parameters
   static RetrievalMethod* createModel(const Index* ind, ArrayAccumulator* accum, RetModel type);
 
   /// use specified model or check in parameters
   static RetrievalMethod* createModel(const Index* ind, ArrayAccumulator* accum, string type = "");
+
+  /// automatically generate retrieval and query models from the given query 
+  /// stopword file and stemmer are optional
+  /// specify model using string type name
+  /// query terms delimited by space
+  /// delete IndexedRealVector
+  static IndexedRealVector* runQuery(const string& query, const Index* index, const string& modeltype, const string& stopfile="", const string& stemtype="", const string& datadir="", const string& func="");
+
+  /// automatically generate query models from the given query and run through
+  /// given retrieval model. stopword file and stemmer are optional
+  /// query terms delimited by space
+  /// delete IndexedRealVector returned
+  static IndexedRealVector* runTextQuery(const string& query, TextQueryRetMethod* model, const string& stopfile="", const string& stemtype="", const string& datadir="", const string& func="");
+
+  /// automatically generate query models from the given query and run through
+  /// given retrieval model. stopword file and stemmer are optional
+  /// query terms delimited by space
+  /// delete IndexedRealVector returned
+  static IndexedRealVector* runStructQuery(const string& query, StructQueryRetMethod* model, const string& stopfile="", const string& stemtype="", const string& datadir="", const string& func="");
+
+
 };
 
 #endif
--- retrieval/src/RetMethodManager.cpp
+++ retrieval/src/RetMethodManager.cpp
@@ -56,6 +56,12 @@ RetrievalMethod* RetMethodManager::createModel (const Index* ind,
     model = new CosSimRetMethod(*ind, CosSimParameter::L2NormFile, *accum);
     ((CosSimRetMethod *)model)->setFeedbackParam(CosSimParameter::fbPrm);
     break;
+  case INQ_STRUCT:
+    InQueryParameter::get();
+    model = new InQueryRetMethod(*ind, InQueryParameter::defaultBelief,
+				 InQueryParameter::fbTermCount,
+				 InQueryParameter::fbCoeff,
+				 InQueryParameter::cacheIDF);
   }  
   return model;
 }
@@ -84,6 +90,124 @@ RetrievalMethod* RetMethodManager::createModel (const Index* ind,
     mod = createModel(ind, accum, CORI_CS);
   if (type == "cos")
     mod = createModel(ind, accum, COS);
+  if (type == "inq_struct")
+    mod = createModel(ind, accum, INQ_STRUCT);
 
   return mod;
+}
+
+IndexedRealVector* RetMethodManager::runQuery(const string& query, const Index* index, const string& modeltype, const string& stopfile, const string& stemtype, const string& datadir, const string& func) {
+  if (!index)
+    LEMUR_THROW(LEMUR_MISSING_PARAMETER_ERROR, "cannot run query without an index");
+
+  ArrayAccumulator accum(index->docCount());
+  IndexedRealVector* results = NULL;
+  RetrievalMethod* model = createModel(index, &accum, modeltype);
+  if (!model)
+    LEMUR_THROW(LEMUR_MISSING_PARAMETER_ERROR, "could not create retrieval model from given type");
+  
+  if (TextQueryRetMethod* textmethod = dynamic_cast<TextQueryRetMethod*>(model)) {    
+    results = runTextQuery(query, textmethod, stopfile, stemtype, datadir, func);
+  } else if (StructQueryRetMethod* structmethod = dynamic_cast<StructQueryRetMethod*>(model)) {
+    results = runStructQuery(query, structmethod, stopfile, stemtype, datadir, func);
+  } else {
+    delete(model);
+    LEMUR_THROW(LEMUR_GENERIC_ERROR, "could not handle retrieval model");
+  }
+  delete(model);
+  return results;
+}
+
+
+IndexedRealVector* RetMethodManager::runTextQuery(const string& query, 
+						  TextQueryRetMethod* model, 
+						  const string& stopfile, 
+						  const string& stemtype, 
+						  const string& datadir, 
+						  const string& func) {
+  Stopper* stopper = TextHandlerManager::createStopper(stopfile);
+  Stemmer* stemmer = TextHandlerManager::createStemmer(stemtype, datadir, func);
+  StringQuery* parser = NULL;
+  StringQuery* qryterms;
+  TextHandler* chain;
+
+  if (stopper || stemmer) {
+    parser = new StringQuery();
+    chain = parser;
+    qryterms = new StringQuery();
+
+    if (stopper) {
+      chain->setTextHandler(stopper);
+      chain=stopper;
+    }
+    
+    if (stemmer) {
+      chain->setTextHandler(stemmer);    
+      chain=stemmer;
+    }
+    
+    chain->setTextHandler(qryterms);
+    parser->parse(query);
+
+    delete(parser);
+    
+  } else {
+    qryterms = new StringQuery(query);
+  }
+  IndexedRealVector* results = new IndexedRealVector();
+  QueryRep* qr = model->computeQueryRep(*qryterms);
+  model->scoreCollection(*qr, *results);
+  results->Sort();
+
+  if (stopper)
+    delete(stopper);
+  if (stemmer)
+    delete(stemmer);
+  delete(qryterms);
+  delete(qr);
+
+  return results;
+}
+
+IndexedRealVector* RetMethodManager::runStructQuery(const string& query, 
+						    StructQueryRetMethod* model, 
+						    const string& stopfile, 
+						    const string& stemtype, 
+						    const string& datadir, 
+						    const string& func) {
+  Stopper* stopper = TextHandlerManager::createStopper(stopfile);
+  Stemmer* stemmer = TextHandlerManager::createStemmer(stemtype, datadir, func);
+  // make bogus query number for parser
+  string structquery = "#q1=" + query;
+  InQueryOpParser* parser = new InQueryOpParser();
+  StringQuery* qryterms = new StringQuery();
+  TextHandler* chain = parser;
+
+  if (stopper) {
+    chain->setTextHandler(stopper);
+    chain=stopper;
+  }
+    
+  if (stemmer) {
+    chain->setTextHandler(stemmer);    
+    chain=stemmer;
+  }
+    
+  chain->setTextHandler(qryterms);
+  parser->parseBuffer((char*)structquery.c_str(), structquery.length());
+
+  IndexedRealVector* results = new IndexedRealVector();
+  QueryRep* qr = model->computeQueryRep(*qryterms);
+  model->scoreCollection(*qr, *results);
+  results->Sort();
+
+  if (stopper)
+    delete(stopper);
+  if (stemmer)
+    delete(stemmer);
+  delete(parser);
+  delete(qryterms);
+  delete(qr);
+
+  return results;
 }
