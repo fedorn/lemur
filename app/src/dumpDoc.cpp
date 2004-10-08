50bbd19761f06276d52e77d4c09e46f0ad0cc37f
dfisher <dfisher>
2004-10-08 16:28:35 +0000

    Add two utility applications.

---
+++ app/src/dumpDoc.cpp
@@ -0,0 +1,46 @@
+/*==========================================================================
+ * Copyright (c) 2003-2004 University of Massachusetts.  All Rights Reserved.
+ *
+ * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
+ * is subject to the terms of the software license set forth in the LICENSE
+ * file included with this software, and also available at
+ * http://www.cs.cmu.edu/~lemur/license.html
+ *
+ *==========================================================================
+*/
+/*
+  author: dmf
+ */
+
+#include "common_headers.hpp"
+#include "IndexManager.hpp"
+#include "Index.hpp"
+
+int main(int argc, char *argv[]) {
+  Index *ind;
+  if (argc < 3) {
+    cerr << "usage: dumpDoc <index_name> <internal/external docid> [-ext]" 
+	 << endl;
+    exit (1);
+  }
+  ind = IndexManager::openIndex(argv[1]);
+  DOCID_T did;
+  if (argc == 3)
+    did = atoi(argv[2]);
+  else did = ind->document(argv[2]);
+  cout << ind->document(did) << ": " << ind->docLength(did) << endl;
+  TermInfoList *tList = ind->termInfoList(did);
+  if (tList == NULL) {
+    cerr << ": empty termInfoList" << endl;
+    exit (1);
+  }
+  TermInfo *info;
+  tList->startIteration();
+  while (tList->hasMore()) {
+    info = tList->nextEntry();
+    cout << ind->term(info->termID()) << ": " << info->count() << endl;
+  }
+  delete tList;
+  delete(ind);
+  return 0;
+}
---
+++ app/src/dumpTerm.cpp
@@ -0,0 +1,55 @@
+/*==========================================================================
+ * Copyright (c) 2003-2004 University of Massachusetts.  All Rights Reserved.
+ *
+ * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
+ * is subject to the terms of the software license set forth in the LICENSE
+ * file included with this software, and also available at
+ * http://www.cs.cmu.edu/~lemur/license.html
+ *
+ *==========================================================================
+*/
+/*
+  author: dmf
+ */
+
+#include "common_headers.hpp"
+#include "IndexManager.hpp"
+#include "Index.hpp"
+int main(int argc, char *argv[]) {
+  Index *ind;
+  if (argc < 3) {
+    cerr << "usage: dumpTerm <index_name> <internal/external termid> [-ext]" 
+	 << endl;
+    exit (1);
+  }
+  
+  ind = IndexManager::openIndex(argv[1]);
+  TERMID_T did;
+  if (argc == 3)
+    did = atoi(argv[2]);
+  else did = ind->term(argv[2]);
+  
+  cout << ind->term(did) << endl;
+  DocInfoList *tList = ind->docInfoList(did);
+  if (tList == NULL) {
+    cerr << ": empty docInfoList" << endl;
+    exit (1);
+  }
+  
+  DocInfo *info;
+  tList->startIteration();
+  while (tList->hasMore()) {
+    info = tList->nextEntry();
+    const LOC_T *pos = info->positions();
+    COUNT_T count = info->termCount();
+    cout << ind->document(info->docID()) << "(" << count << "): ";
+    if (pos != NULL) {
+      for (COUNT_T i = 0; i < count; i++)
+	cout << pos[i] << " ";
+    }
+    cout << endl;
+  }
+  delete tList;
+  delete(ind);
+  return 0;
+}
