c6ad1ea69630a2dcaf6839aa9d5314533a49f302
dfisher <dfisher>
2004-10-13 14:29:45 +0000

    Fixed a bug in WeightFoldingCopier where it wasn't normalizing weights properly before folding them, which messed up query expansion.

--- retrieval/include/indri/WeightFoldingCopier.hpp
+++ retrieval/include/indri/WeightFoldingCopier.hpp
@@ -30,18 +30,33 @@ public:
       // is this a weight node?
       indri::lang::WeightNode* childWeightNode = dynamic_cast<indri::lang::WeightNode*>( children[i].second );
 
-      if( !childWeightNode ) {
+      // is this a combine node?
+      indri::lang::CombineNode* childCombineNode = dynamic_cast<indri::lang::CombineNode*>( children[i].second );
+
+      if( !childWeightNode && !childCombineNode ) {
         // child is not a weight node, so just add it directly
         newerWeightNode->addChild( children[i].first, children[i].second );
+      } else if( childCombineNode ) {
+        const std::vector< indri::lang::ScoredExtentNode* >& grandkids = childCombineNode->getChildren();
+        double kidWeight = children[i].first / double(grandkids.size());
+        
+        for( size_t j=0; j<grandkids.size(); j++ ) {
+          newerWeightNode->addChild( kidWeight, grandkids[j] );
+        }
       } else {
         // child _is_ a weight node, so we're going to fold all its children up to this level
         const std::vector< std::pair<double, indri::lang::ScoredExtentNode*> >& grandkids = childWeightNode->getChildren();
         double parentWeight = children[i].first;
+        double normalizer = 0.0;
+
+        // need to normalize all weights to sum to 1
+        for( size_t j=0; j<grandkids.size(); j++ ) {
+          normalizer += grandkids[j].first;
+        }
     
         for( size_t j=0; j<grandkids.size(); j++ ) {
           // have to normalize the weight by including the parent weight as well
-          // assumed precondition: child weights normalized to 1
-          newerWeightNode->addChild( parentWeight * grandkids[j].first,
+          newerWeightNode->addChild( parentWeight * grandkids[j].first / normalizer,
                                      grandkids[j].second );
         }
       }
