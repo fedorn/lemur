c867028c7b5aa44c01b94c73e3e874fd1b56eec6
tnt <tnt>
2004-10-21 22:28:46 +0000

    update license URL

--- cluster/include/ThresholdFcn.hpp
+++ cluster/include/ThresholdFcn.hpp
@@ -4,7 +4,7 @@
  * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
  * is subject to the terms of the software license set forth in the LICENSE
  * file included with this software, and also available at
- * http://www.cs.cmu.edu/~lemur/license.html
+ * http://www.lemurproject.org/license.html
  *
  *==========================================================================
 */
