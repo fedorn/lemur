7c76d6e126c631002001c14b6f5377d6753725bd
dfisher <dfisher>
2005-04-20 15:52:03 +0000

    Fix BUGIN #65, yyless before incrementing chinesepos.

--- parsing/src/ChineseParser.cpp
+++ parsing/src/ChineseParser.cpp
@@ -738,7 +738,7 @@ YY_MALLOC_DECL
 YY_DECL
 	{
 	register yy_state_type yy_current_state;
-	register char *yy_cp = NULL, *yy_bp = NULL;
+	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
 #line 58 "../src/ChineseParser.l"
@@ -925,7 +925,7 @@ YY_RULE_SETUP
 case 20:
 YY_RULE_SETUP
 #line 79 "../src/ChineseParser.l"
-{ chinesepos += Chineseleng; yyless(2); /* zap one chinese punctutions */ } 
+{ yyless(2); chinesepos += Chineseleng;  /* zap one chinese punctutions */ } 
 	YY_BREAK
 case 21:
 YY_RULE_SETUP
@@ -1355,7 +1355,6 @@ register char *yy_bp;
 #endif	/* ifndef YY_NO_UNPUT */
 
 
-#ifndef YY_NO_INPUT
 #ifdef __cplusplus
 static int yyinput()
 #else
@@ -1427,7 +1426,7 @@ static int input()
 
 	return c;
 	}
-#endif /* YY_NO_INPUT */
+
 
 #ifdef YY_USE_PROTOS
 void yyrestart( FILE *input_file )
@@ -1538,6 +1537,11 @@ YY_BUFFER_STATE b;
 	}
 
 
+#ifndef YY_ALWAYS_INTERACTIVE
+#ifndef YY_NEVER_INTERACTIVE
+extern int isatty YY_PROTO(( int ));
+#endif
+#endif
 
 #ifdef YY_USE_PROTOS
 void yy_init_buffer( YY_BUFFER_STATE b, FILE *file )
