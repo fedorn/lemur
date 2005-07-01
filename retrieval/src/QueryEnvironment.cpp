f1195a83e89ce86d2999fd84b0abd7602780f55e
dfisher <dfisher>
2005-07-01 12:00:00 +0000

    Insert the stopword parameters into the stopper slice rather than toplevel enabling query side stopping.

--- retrieval/src/QueryEnvironment.cpp
+++ retrieval/src/QueryEnvironment.cpp
@@ -160,7 +160,8 @@ void indri::api::QueryEnvironment::setStopwords( const std::vector<std::string>&
   _parameters.set("stopper","");
   Parameters p = _parameters.get("stopper");
   for( unsigned int i=0; i<stopwords.size(); i++ ) {
-    _parameters.append("word").set(stopwords[i]);
+    // use the stopper slice
+    p.append("word").set(stopwords[i]);
   }
 }
 
