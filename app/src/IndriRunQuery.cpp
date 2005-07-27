f93d7bc3580a5457a148ef29f858260d7b5e5550
dfisher <dfisher>
2005-07-27 17:20:26 +0000

    Update to threaded version. Adds use of number element in queries (as an alternative to queryOffset). Adds potential to improve performance via running multiple retrieval threads.

--- app/src/IndriRunQuery.cpp
+++ app/src/IndriRunQuery.cpp
@@ -172,31 +172,11 @@ as <tt>-fbOrigWeight=number</tt> on the command line.</dd>
 #include "indri/PonteExpander.hpp"
 
 #include "indri/IndriTimer.hpp"
+#include "indri/UtilityThread.hpp"
+#include "indri/ScopedLock.hpp"
+#include "indri/delete_range.hpp"
 
-#ifndef WIN32
-#include <sys/time.h>
-#endif
-
-void open_indexes( indri::api::Parameters& params, indri::api::QueryEnvironment& env ) {
-  if( !params.exists("index") && !params.exists("server") ) 
-    LEMUR_THROW( LEMUR_MISSING_PARAMETER_ERROR, "Must specify a server or index to query against." );
-
-  if( params.exists( "index" ) ) {
-    indri::api::Parameters indexes = params["index"];
-
-    for( unsigned int i=0; i < indexes.size(); i++ ) {
-      env.addIndex( std::string(indexes[i]) );
-    }
-  }
-
-  if( params.exists( "server" ) ) {
-    indri::api::Parameters servers = params["server"];
-
-    for( unsigned int i=0; i < servers.size(); i++ ) {
-      env.addServer( std::string(servers[i]) );
-    }
-  }
-}
+#include <queue>
 
 static bool copy_parameters_to_string_vector( std::vector<std::string>& vec, indri::api::Parameters p, const std::string& parameterName ) {
   if( !p.exists(parameterName) )
@@ -211,6 +191,246 @@ static bool copy_parameters_to_string_vector( std::vector<std::string>& vec, ind
   return true;
 }
 
+struct query_t {
+  struct greater {
+    bool operator() ( query_t* one, query_t* two ) {
+      return one->index > two->index;
+    }
+  };
+
+  query_t( int _index, int _number, const std::string& _text ) :
+    index( _index ),
+    number( _number ),
+    text( _text )
+  {
+  }
+
+  int number;
+  int index;
+  std::string text;
+};
+
+class QueryThread : public indri::thread::UtilityThread {
+private:
+  indri::thread::Lockable& _queueLock;
+  std::queue< query_t* >& _queries;
+  std::priority_queue< query_t*, std::vector< query_t* >, query_t::greater >& _output;
+
+  indri::api::QueryEnvironment _environment;
+  indri::api::Parameters& _parameters;
+  int _requested;
+  int _initialRequested;
+
+  bool _printDocuments;
+  bool _printPassages;
+  bool _printQuery;
+
+  std::string _runID;
+  bool _trecFormat;
+    
+  indri::query::QueryExpander* _expander;
+  std::vector<indri::api::ScoredExtentResult> _results;
+
+  // Runs the query, expanding it if necessary.  Will print output as well if verbose is on.
+  void _runQuery( std::stringstream& output, const std::string& query ) {
+    if( _printQuery ) output << "# query: " << query << std::endl;
+
+    _results = _environment.runQuery( query, _initialRequested );
+
+    if( _expander ) {
+      std::string expandedQuery = _expander->expand( query, _results );
+      if( _printQuery ) output << "# expanded: " << expandedQuery << std::endl;
+      _results = _environment.runQuery( expandedQuery, _requested );
+    }
+  }
+
+  void _printResults( std::stringstream& output, int queryIndex ) {
+    std::vector<std::string> documentNames;
+    std::vector<indri::api::ParsedDocument*> documents;
+
+    // Fetch document data for printing
+    if( _printDocuments || _printPassages ) {
+      // Need document text, so we'll fetch the whole document
+      documents = _environment.documents( _results );
+      documentNames.clear();
+
+      for( unsigned int i=0; i<_results.size(); i++ ) {
+        indri::api::ParsedDocument* doc = documents[i];
+        std::string documentName;
+
+        indri::utility::greedy_vector<indri::parse::MetadataPair>::iterator iter = std::find_if( documents[i]->metadata.begin(),
+                                                                                                 documents[i]->metadata.end(),
+                                                                                                 indri::parse::MetadataPair::key_equal( "docno" ) );
+
+        if( iter != documents[i]->metadata.end() )
+          documentName = (char*) iter->value;
+
+        // store the document name in a separate vector so later code can find it
+        documentNames.push_back( documentName );
+      }
+    } else {
+      // We only want document names, so the documentMetadata call may be faster
+      documentNames = _environment.documentMetadata( _results, "docno" );
+    }
+
+    // Print results
+    for( unsigned int i=0; i < _results.size(); i++ ) {
+      int rank = i+1;
+      int queryNumber = queryIndex;
+
+      if( _trecFormat ) {
+        // TREC formatted output: queryNumber, Q0, documentName, rank, score, runID
+        output << queryNumber << " "
+               << "Q0 "
+               << documentNames[i] << " "
+               << rank << " "
+               << _results[ i ].score << " "
+               << _runID << std::endl;
+      }
+      else {
+        // score, documentName, firstWord, lastWord
+        output << _results[i].score << "\t"
+               << documentNames[i] << "\t"
+               << _results[i].begin << "\t"
+               << _results[i].end << std::endl;
+      }
+
+      if( _printDocuments ) {
+        output << documents[i]->text << std::endl;
+      }
+
+      if( _printPassages ) {
+        // we'll print the text from the beginning of the first word
+        int byteBegin = documents[i]->positions[ _results[i].begin ].begin;
+        int byteEnd = documents[i]->positions[ _results[i].end-1 ].end;
+        output.write( documents[i]->text + byteBegin, byteEnd - byteBegin );
+        output << std::endl;
+      }
+
+      if( documents.size() )
+        delete documents[i];
+    }
+  }
+
+public:
+  QueryThread( std::queue< query_t* >& queries,
+               std::priority_queue< query_t*, std::vector< query_t* >, query_t::greater >& output,
+               indri::thread::Lockable& queueLock,
+               indri::api::Parameters& params ) :
+    _queries(queries),
+    _output(output),
+    _queueLock(queueLock),
+    _parameters(params),
+    _expander(0)
+  {
+  }
+
+  ~QueryThread() {
+  }
+
+  UINT64 initialize() {
+    _environment.setMemory( _parameters.get("memory", 100*1024*1024) );
+
+    std::vector<std::string> stopwords;
+    if( copy_parameters_to_string_vector( stopwords, _parameters, "stopper.word" ) )
+      _environment.setStopwords(stopwords);
+    
+    std::vector<std::string> smoothingRules;
+    if( copy_parameters_to_string_vector( smoothingRules, _parameters, "rule" ) )
+      _environment.setScoringRules( smoothingRules );
+
+    if( _parameters.exists( "index" ) ) {
+      indri::api::Parameters indexes = _parameters["index"];
+
+      for( unsigned int i=0; i < indexes.size(); i++ ) {
+        _environment.addIndex( std::string(indexes[i]) );
+      }
+    }
+
+    if( _parameters.exists( "server" ) ) {
+      indri::api::Parameters servers = _parameters["server"];
+
+      for( unsigned int i=0; i < servers.size(); i++ ) {
+        _environment.addServer( std::string(servers[i]) );
+      }
+    }
+
+    _requested = _parameters.get( "count", 1000 );
+    _initialRequested = _parameters.get( "fbDocs", _requested );
+    _runID = _parameters.get( "runID", "indri" );
+    _trecFormat = _parameters.get( "trecFormat" , false );
+    _printQuery = _parameters.get( "printQuery", false );
+    _printDocuments = _parameters.get( "printDocuments", false );
+    _printPassages = _parameters.get( "printPassages", false );
+
+    if( _parameters.get( "fbDocs", 0 ) != 0 ) {
+      _expander = new indri::query::RMExpander( &_environment, _parameters );
+    }
+
+    return 0;
+  }
+
+  void deinitialize() {
+    delete _expander;
+    _environment.close();
+  }
+
+  bool hasWork() {
+    indri::thread::ScopedLock sl( &_queueLock );
+    return _queries.size() > 0;
+  }
+
+  UINT64 work() {
+    query_t* query;
+    std::stringstream output;
+
+    // pop a query off the queue
+    {
+      indri::thread::ScopedLock sl( &_queueLock );
+      if( _queries.size() ) {
+        query = _queries.front();
+        _queries.pop();
+      } else {
+        return 0;
+      }
+    }
+
+    // run the query
+    _runQuery( output, query->text );
+
+    // print the results to the output stream
+    _printResults( output, query->number );
+
+    // push that data into an output queue...?
+    {
+      indri::thread::ScopedLock sl( &_queueLock );
+      _output.push( new query_t( query->index, query->number, output.str() ) );
+    }
+
+    delete query;
+    return 0;
+  }
+};
+
+void push_queue( std::queue< query_t* >& q, indri::api::Parameters& queries,
+                 int queryOffset ) {
+
+  for( int i=0; i<queries.size(); i++ ) {
+    int queryNumber;
+    std::string queryText;
+
+    if( queries[i].exists( "number" ) ) {
+      queryText = (std::string) queries[i]["text"];
+      queryNumber = (int) queries[i]["number"];
+    } else {
+      queryText = (std::string) queries[i];
+      queryNumber = queryOffset + i;
+    }
+    
+    q.push( new query_t( i, queryNumber, queryText ) );
+  }
+}
+
 int main(int argc, char * argv[]) {
   try {
     indri::api::Parameters& param = indri::api::Parameters::instance();
@@ -223,116 +443,57 @@ int main(int argc, char * argv[]) {
     if( !param.exists( "query" ) )
       LEMUR_THROW( LEMUR_MISSING_PARAMETER_ERROR, "Must specify at least one query." );
 
-    std::string queryString = param["query"];
-    int resultsRequested = param.get( "count", 1000 );
-    int initialRequested = param.get( "fbDocs", resultsRequested );
-    int fbDocs = param.get( "fbDocs", 0 );
-    string runID = param.get( "runID", "indri" );
-    int queryOffset = param.get( "queryOffset" , 0 );
-    int trecFormat = param.get( "trecFormat" , 0 );
-    bool printQuery = param.get( "printQuery", false );
+    if( !param.exists("index") && !param.exists("server") ) 
+      LEMUR_THROW( LEMUR_MISSING_PARAMETER_ERROR, "Must specify a server or index to query against." );
 
-    indri::api::Parameters queries = param[ "query" ];
+    int threadCount = param.get( "threads", 1 );
+    std::queue< query_t* > queries;
+    std::priority_queue< query_t*, std::vector< query_t* >, query_t::greater > output;
+    std::vector< QueryThread* > threads;
+    indri::thread::Mutex queueLock;
 
-    indri::api::QueryEnvironment env;
+    // push all queries onto a queue
+    indri::api::Parameters parameterQueries = param[ "query" ];
+    int queryOffset = param.get( "queryOffset", 0 );
+    push_queue( queries, parameterQueries, queryOffset );
+    int queryCount = queries.size();
 
-    env.setMemory( param.get("memory", 100*1024*1024) );
-
-    std::vector<std::string> stopwords;
-    if( copy_parameters_to_string_vector( stopwords, param, "stopper.word" ) )
-      env.setStopwords(stopwords);
-    
-    std::vector<std::string> smoothingRules;
-    if( copy_parameters_to_string_vector( smoothingRules, param, "rule" ) )
-      env.setScoringRules( smoothingRules );
-    // open the indexes after the query environment parameters have been set.
-    open_indexes( param, env );
-
-    indri::query::QueryExpander * qe = NULL;
-
-    if( fbDocs ) {
-      qe = new indri::query::RMExpander( &env, param );
+    // launch threads
+    for( int i=0; i<threadCount; i++ ) {
+      threads.push_back( new QueryThread( queries, output, queueLock, param ) );
+      threads.back()->start();
     }
-    // actually perform the query (in parallel for servers, in series for
-    // the local databases)
-    std::vector<indri::api::ScoredExtentResult> results;
 
-    // fetch document information
-    std::vector<std::string> documentNames;
-    std::vector<indri::api::ParsedDocument*> documents;
+    int query = 0;
 
-    for( int query = 0; query < queries.size(); query++ ) {
-      std::string queryString = std::string( queries[ query ] );
-      std::string expandedQuery;
-
-      if( printQuery ) std::cout << "# query: " << queryString << std::endl;
-
-      results = env.runQuery( queryString, initialRequested );
-
-      if( qe ) {
-        expandedQuery = qe->expand( queryString, results );
-        if( printQuery ) std::cout << "# expanded: " << expandedQuery << std::endl;
-        results = env.runQuery( expandedQuery, resultsRequested );
+    // process output as it appears on the queue
+    while( query < queryCount ) {
+      query_t* result = NULL;
+      
+      {
+        indri::thread::ScopedLock sl( queueLock );
+          
+        if( output.size() && output.top()->index == query ) {
+          result = output.top();
+          output.pop();
+        }
       }
 
-      if( param.get( "printDocuments", 0 ) || param.get( "printPassages", 0 ) ) {
-        documents = env.documents( results );
-        documentNames.clear();
-
-        for( unsigned int i=0; i<results.size(); i++ ) {
-          indri::api::ParsedDocument* doc = documents[i];
-          std::string documentName;
-
-          indri::utility::greedy_vector<indri::parse::MetadataPair>::iterator iter = std::find_if( documents[i]->metadata.begin(),
-            documents[i]->metadata.end(),
-            indri::parse::MetadataPair::key_equal( "docno" ) );
-
-          if( iter != documents[i]->metadata.end() )
-            documentName = (char*) iter->value;
-
-          documentNames.push_back( documentName );
-        }
+      if( result ) {
+        std::cout << result->text;
+        delete result;
+        query++;
       } else {
-        documentNames = env.documentMetadata( results, "docno" );
-      }
-
-      // print results: <score> <name> <beginExtent> <endExtent>
-      for( unsigned int i=0; i<results.size(); i++ ) {
-
-        if( trecFormat ) {
-          std::cout << ( query + queryOffset + 1 ) << " "
-            << "Q0 "
-            << documentNames[i] << " "
-            << ( i + 1 ) << " "
-            << results[ i ].score << " "
-            << runID << std::endl;
-        }
-        else {
-          std::cout << results[i].score << "\t"
-            << documentNames[i] << "\t"
-            << results[i].begin << "\t"
-            << results[i].end << std::endl;
-        }
-
-        if( param.get( "printDocuments", 0 ) ) {
-          std::cout << documents[i]->text << std::endl;
-        }
-
-        if( param.get( "printPassages", 0 ) ) {
-          // we'll print the text from the beginning of the first word
-          int byteBegin = documents[i]->positions[ results[i].begin ].begin;
-          int byteEnd = documents[i]->positions[ results[i].end-1 ].end;
-          std::cout.write( documents[i]->text + byteBegin, byteEnd - byteBegin );
-          std::cout << std::endl;
-        }
-
-        if( documents.size() )
-          delete documents[i];
+        indri::thread::Thread::yield();
       }
     }
 
-    delete qe;
-    env.close();
+    // join all the threads
+    for( int i=0; i<threads.size(); i++ )
+      threads[i]->join();
+
+    // we've seen all the query output now, so we can quit
+    indri::utility::delete_vector_contents( threads );
   } catch( lemur::api::Exception& e ) {
     LEMUR_ABORT(e);
   } catch( ... ) {
@@ -342,5 +503,3 @@ int main(int argc, char * argv[]) {
 
   return 0;
 }
-
-
