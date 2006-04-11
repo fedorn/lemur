4966f21fb268bebb5cb3859cbe1ec541f536072b
pto <pto>
2006-04-11 20:09:50 +0000

    Added ordinals and sort functions

--- utility/include/indri/Extent.hpp
+++ utility/include/indri/Extent.hpp
@@ -27,10 +27,13 @@ namespace indri
       double weight;
       int begin;
       int end;
+      int ordinal;
 
-      Extent() : weight(1), begin(-1), end(-1) {}
-      Extent( int b, int e ) : weight(1), begin(b), end(e) {}
-      Extent( double w, int b, int e ) : weight(w), begin(b), end(e) {}
+      Extent() : weight(1), begin(-1), end(-1), ordinal(0) {}
+      Extent( int b, int e ) : weight(1), begin(b), end(e), ordinal(0) {}
+      Extent( int b, int e, int o ) : weight(1), begin(b), end(e), ordinal(o) {}
+      Extent( double w, int b, int e ) : weight(w), begin(b), end(e), ordinal(0) {}
+      Extent( double w, int b, int e, int o) : weight(w), begin(b), end(e), ordinal(o) {}
     
       bool contains( const Extent& other ) const {
         return begin <= other.begin && end >= other.end;
@@ -47,6 +50,61 @@ namespace indri
       struct begins_before_less {
         bool operator() ( const Extent& one, const Extent& two ) const {
           return one.beginsBefore( two );
+        }
+      };
+      
+      struct ends_before_less {
+	bool operator() ( const Extent& one, const Extent& two ) const {
+	  if ( one.end < two.end ) {
+	    return true;
+	  } else if ( one.end == two.end ) {
+	    if ( one.begin < two.begin ) {
+	      return true;
+	    } else if ( one.begin == two.begin ) {
+	      if (one.ordinal < two.ordinal ) {
+		return true;
+	      } else if ( one.ordinal == two.ordinal ) {
+		if ( one.weight < two.weight ) {
+		  return true;
+		}
+	      } 
+	    }
+	  }
+	  return false;
+	}	
+      };
+
+      struct ends_before_greater {
+	bool operator() ( const Extent& one, const Extent& two ) const {
+	  if ( one.end > two.end ) {
+	    return true;
+	  } else if ( one.end == two.end ) {
+	    if ( one.begin < two.begin ) {
+	      return true;
+	    } else if ( one.begin == two.begin ) {
+	      if (one.ordinal < two.ordinal ) {
+		return true;
+	      } else if ( one.ordinal == two.ordinal ) {
+		if ( one.weight < two.weight ) {
+		  return true;
+		}
+	      } 
+	    }
+	  }
+	  return false;
+	}	
+      };
+
+      struct begins_before_ends_before_less {
+        bool operator() ( const Extent& one, const Extent& two ) const {
+	  if ( one.beginsBefore( two ) )
+	    return true;
+	  else if ( one.begin == two.begin && one.end < two.end ) 
+	    return true;
+	  else if ( one.begin == two.begin && one.end == two.end && one.ordinal < two.ordinal )
+	    return true;
+	  
+	  return false;
         }
       };
     };
