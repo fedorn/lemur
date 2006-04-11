b896b9c97d8cf61694273baf759254c86b4df1bd
pto <pto>
2006-04-11 19:25:59 +0000

    Added NoContextCoungGraphCopier, which strips out context.  This way, one can build a single background language model for the corpus quickly.

--- retrieval/include/indri/ContextCountGraphCopier.hpp
+++ retrieval/include/indri/ContextCountGraphCopier.hpp
@@ -67,6 +67,59 @@ namespace indri
         return _contextCounters;
       }
     };
+
+    class NoContextCountGraphCopier : public indri::lang::Copier {
+    private:
+      std::vector<indri::lang::Node*> _newNodes;
+      std::vector<indri::lang::ContextCounterNode*> _contextCounters;
+
+    public:
+      ~NoContextCountGraphCopier() {
+        indri::utility::delete_vector_contents<indri::lang::Node*>( _newNodes );
+      }
+
+      indri::lang::Node* defaultAfter( indri::lang::Node* oldNode, indri::lang::Node* newNode ) {
+        _newNodes.push_back( newNode );
+        return newNode;
+      }
+
+      indri::lang::Node* after( indri::lang::RawScorerNode* oldNode, indri::lang::RawScorerNode* newNode ) {
+        indri::lang::RawExtentNode* raw = newNode->getRawExtent();
+        indri::lang::RawExtentNode* context = newNode->getContext();
+        delete newNode;
+	context = 0;
+	
+        indri::lang::ContextCounterNode* contextCounter = new indri::lang::ContextCounterNode( raw, context );
+        // need to match the name of the raw scorer we're counting for
+        contextCounter->setNodeName( oldNode->nodeName() );
+        _newNodes.push_back( contextCounter );
+        _contextCounters.push_back( contextCounter );
+        return contextCounter;
+      }
+
+      indri::lang::Node* after( indri::lang::NestedRawScorerNode* oldNode, indri::lang::NestedRawScorerNode* newNode ) {
+	return after ( (indri::lang::RawScorerNode*) oldNode, (indri::lang::RawScorerNode*) newNode );
+      }
+
+      indri::lang::Node* after( indri::lang::ShrinkageScorerNode* oldNode, indri::lang::ShrinkageScorerNode* newNode ) {
+	return after ( (indri::lang::RawScorerNode*) oldNode, (indri::lang::RawScorerNode*) newNode );
+      }
+
+
+      indri::lang::Node* after( indri::lang::ExtentRestriction* oldNode, indri::lang::ExtentRestriction* newNode ) {
+	indri::lang::Node * child = newNode->getChild();
+	delete newNode;
+	return child;
+      }
+
+      std::vector<indri::lang::Node*>& getNodes() {
+        return _newNodes;
+      }
+
+      std::vector<indri::lang::ContextCounterNode*>& getContextCounterNodes() {
+        return _contextCounters;
+      }
+    };
   }
 }
 
