382a70ac33c036b92e92935a535f5de6f7e6e8f7
pto <pto>
2006-04-11 18:55:14 +0000

    Added some additional helper functions for working with the document structure.

--- index/include/indri/DocumentStructure.hpp
+++ index/include/indri/DocumentStructure.hpp
@@ -8,6 +8,7 @@
 #include "indri/greedy_vector"
 #include <sstream>
 #include <vector>
+#include <set>
 
 typedef struct ninf {
   int id;
@@ -62,6 +63,7 @@ public:
 
   void loadStructure(const indri::utility::greedy_vector<indri::index::FieldExtent> & fields);
   void setIndex( indri::index::Index & index );
+  indri::index::Index * getIndex() { return _index; }
 
   ~DocumentStructure();
 
@@ -73,23 +75,38 @@ public:
   int type(int node);
 
   typedef indri::utility::greedy_vector<int>::iterator child_iterator;
+  // Gets the start of a list containing children of a node.  childrenBegin( 0 )
+  // returns the start of a list containing the roots of trees in the document.
   child_iterator childrenBegin( int node );
+  // Gets the end of a list containing children of a node. childrenEnd( 0 )
+  // returns the end of the list containing the roots of trees in the document.
   child_iterator childrenEnd( int node );
   
 
   int nodeCount() { return _numNodes; }
 
+  // Finds a the smallest leaf in a subtree that contains the extent.
+  // By default searches the tree rooted by node 1 (BROKEN)
+  int findLeaf(int begin, int end, int root = -1);
 
-  int findLeaf(int begin, int end);
+  // Finds the deepest nodess that contain the extent.
+  // If exact is true, then the leaf must exactly match (rather than contain) the extent.
+  // Caller is responsible for deleting the returned set.
+  std::set<int> * findLeafs(int begin, int end, bool exact = false);
+  void findLeafs(std::set<int> * leafs, int begin, int end, bool exact = false);
 
   // returns true if node == possibleAncestor or
   // if possibleAncestor is a parent or parent's parent ... of node
   bool ancestor(int node, int possibleAncestor);
 
   std::string path(int node);
+  // returns the ordinal matching a string, 0 if unable to find the ordinal matching the xpath
   int fieldId( const std::string path );
 
-
+  // builds an ordering of nodes that allows walking the trees in a breadth first fashion
+  indri::utility::greedy_vector<int> & topDownOrder();
+  void topDownOrder(std::set<int> & roots, indri::utility::greedy_vector<int> & order);
+  
 protected:
 
   int _numNodes;
@@ -100,8 +117,17 @@ protected:
   Index * _index;
   void _constructNodePath(std::stringstream & path, int node);
   
+  indri::utility::greedy_vector<int> _topDownOrder;
+  // builds a breadth first walk and stores it in  _topDownOrder
+  void _computeTopDownOrder();
+
+  // push leafs matching the extent rooted at node into the set. 
+  // will not insert a node if a descedant matches
+  bool _findLeafs(int node, int begin, int end, bool exact, std::set<int> * leafs);
+
 };
 
   }
 }
 #endif
+
