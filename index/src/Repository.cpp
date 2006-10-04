5b8933f757cc665432f4618023a3bc818e1f0a97
dfisher <dfisher>
2006-10-04 16:55:04 +0000

    Add URLTextAnnotator to the transformations pipeline. Adds tokenized terms for url, urldomain, and urlpath to ParsedDocuments. Indexes of documents containing a url metadata field (eg trecweb) will have different term statistics due to the injection of these terms.

--- index/src/Repository.cpp
+++ index/src/Repository.cpp
@@ -26,6 +26,7 @@
 #include "indri/Parameters.hpp"
 #include "indri/StemmerFactory.hpp"
 #include "indri/NormalizationTransformation.hpp"
+#include "indri/URLTextAnnotator.hpp"
 #include "indri/UTF8CaseNormalizationTransformation.hpp"
 #include "Exception.hpp"
 #include "indri/Thread.hpp"
@@ -131,6 +132,10 @@ void indri::collection::Repository::_buildFields() {
 //
 
 void indri::collection::Repository::_buildChain( indri::api::Parameters& parameters, indri::api::Parameters* options ) {
+  // Extract url from metadata before case normalizing.
+  // this could be parameterized.
+  _transformations.push_back(new indri::parse::URLTextAnnotator());
+
   bool dontNormalize = parameters.exists( "normalize" ) && ( false == (bool) parameters["normalize"] );
 
   if( dontNormalize == false ) {
