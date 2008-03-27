aed1dc769a74a94b24466bc65680ea5e43e9bd2c
david_fisher <david_fisher>
2008-03-27 16:35:09 +0000

    BUG# 1927244 Need to use <= numNodes, as the count is 1-based.

--- retrieval/src/ShrinkageBeliefNode.cpp
+++ retrieval/src/ShrinkageBeliefNode.cpp
@@ -226,7 +226,7 @@ void indri::infnet::ShrinkageBeliefNode::_buildScoreCache( int documentID ) {
       activeOuterExtents.erase( activeOuterExtents.begin(), activeIter );         
 
       // push new document node extents on that we may need
-      while ( docNode < numNodes && docStruct->begin( docNode ) <= innerIter->begin ) {      
+      while ( docNode <= numNodes && docStruct->begin( docNode ) <= innerIter->begin ) {      
         indri::index::Extent extent( 1.0, docStruct->begin( docNode ), docStruct->end( docNode ), docNode );
         // only insert if needed
         if ( extent.end >= innerIter->begin ) {
