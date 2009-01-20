f882753cefe51a28485faded7b27843e5aea6eea
david_fisher <david_fisher>
2009-01-20 15:32:03 +0000

    Update indri version number to 2.8.

--- utility/include/indri/indri-platform.h
+++ utility/include/indri/indri-platform.h
@@ -32,7 +32,7 @@
 #endif /* __APPLE__ */
 
 #define INDRID_PORT ( 16743 )
-#define INDRI_DISTRIBUTION "Indri development release 2.7"
+#define INDRI_DISTRIBUTION "Indri development release 2.8"
 
 #endif /* INDRI_PLATFORM_H */
 
