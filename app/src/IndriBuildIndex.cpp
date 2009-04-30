ef746707e6b1e48419701dc72ca417b2699670be
david_fisher <david_fisher>
2009-04-30 15:19:30 +0000

    Feature Request# 2773004 -- Add documentation string for warcchar file class environment.

--- app/src/IndriBuildIndex.cpp
+++ app/src/IndriBuildIndex.cpp
@@ -58,6 +58,7 @@ line. The known classes are:
 <li>trectext -- TREC format, eg TREC-3 onward.
 <li>trecalt -- TREC format, eg TREC-3 onward, with only the TEXT field included.
 <li>warc -- WARC (Web ARChive) format, such as is output by the Nutch webcrawler.
+<li>warcchar -- WARC (Web ARChive) format, such as is output by the Nutch webcrawler. Tokenizes individual characters, enabling indexing of unsgemented text.
 <li>doc -- Microsoft Word format (windows platform only).
 <li>ppt -- Microsoft Powerpoint format (windows platform only).
 <li>pdf --  Adobe PDF format.
@@ -363,6 +364,7 @@ line. The known classes are:
 <li>trectext -- TREC format, eg TREC-3 onward.
 <li>trecalt -- TREC format, eg TREC-3 onward, with only the TEXT field included.
 <li>warc -- WARC (Web ARChive) format, such as is output by the Nutch webcrawler.
+<li>warcchar -- WARC (Web ARChive) format, such as is output by the Nutch webcrawler. Tokenizes individual characters, enabling indexing of unsgemented text.
 <li>doc -- Microsoft Word format (windows platform only).
 <li>ppt -- Microsoft Powerpoint format (windows platform only).
 <li>pdf --  Adobe PDF format.
