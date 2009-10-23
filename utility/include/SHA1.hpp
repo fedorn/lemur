b579c8c9269f24939c2eaa5f02d127d4e3abc616
david_fisher <david_fisher>
2009-10-23 13:13:14 +0000

    Add copyright notice.

--- utility/include/SHA1.hpp
+++ utility/include/SHA1.hpp
@@ -1,3 +1,15 @@
+/*==========================================================================
+ * Copyright (c) 2004-2008 Carnegie Mellon University and University of
+ * Massachusetts.  All Rights Reserved.
+ *
+ * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
+ * is subject to the terms of the software license set forth in the LICENSE
+ * file included with this software, and also available at
+ * http://www.lemurproject.org/license.html
+ *
+ *==========================================================================
+*/
+
 #ifndef _SHA1_HPP
 #define _SHA1_HPP
 
--- utility/src/SHA1.cpp
+++ utility/src/SHA1.cpp
@@ -1,3 +1,15 @@
+/*==========================================================================
+ * Copyright (c) 2004-2008 Carnegie Mellon University and University of
+ * Massachusetts.  All Rights Reserved.
+ *
+ * Use of the Lemur Toolkit for Language Modeling and Information Retrieval
+ * is subject to the terms of the software license set forth in the LICENSE
+ * file included with this software, and also available at
+ * http://www.lemurproject.org/license.html
+ *
+ *==========================================================================
+*/
+
 #include "SHA1.hpp"
 #include "string.h"
 
