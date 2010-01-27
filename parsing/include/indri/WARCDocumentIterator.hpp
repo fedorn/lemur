1cf46cf4c9a4b6baf85d406584ceee520931d3ea
david_fisher <david_fisher>
2010-01-27 14:28:05 +0000

    BUG# 2939370 -- Bring the iterator more in line with the spec. Ignore records that are not of WARC-Type: response. Handles both ClueWeb09 and heritrix generated (sample WARC/0.17) files.

--- parsing/include/indri/WARCDocumentIterator.hpp
+++ parsing/include/indri/WARCDocumentIterator.hpp
@@ -18,41 +18,78 @@
 
 #ifndef INDRI_WARCDOCUMENTITERATOR_HPP
 #define INDRI_WARCDOCUMENTITERATOR_HPP
-
 #include <string>
 #include <fstream>
 #include "zlib.h"
 #include "indri/DocumentIterator.hpp"
 #include "indri/Buffer.hpp"
 #include "indri/UnparsedDocument.hpp"
+#include "indri/HashTable.hpp"
 
 namespace indri
 {
   namespace parse
   {
-    
+    class WARCRecord {
+      private:
+      // header fields
+      //WARC-TYPE
+      std::string warcType;
+      //WARC-Record-ID
+      std::string uuid;
+      // WARC-TREC-ID // clueweb specific
+      std::string trecID;
+      // WARC-Target-URI
+      std::string targetURI;
+      //Content-Length
+      int contentLength;
+      // other metadata headers
+      indri::utility::HashTable< std::string, std::string > metadata;
+      // the header of the record
+      std::string header;
+      // the body of the record
+      const char *content;
+      bool _readLine( char*& beginLine, size_t& lineLength );
+      bool readHeader();
+      bool readContent();
+      gzFile &_gzin;
+      indri::utility::Buffer & _buffer;
+      public:
+      WARCRecord(gzFile &in, indri::utility::Buffer &buf) : _gzin(in), 
+                                                            _buffer(buf) { }
+
+      ~WARCRecord();
+
+      std::string getWarcType() { return warcType ; }
+      std::string getUUID() { return uuid; }
+      std::string getTrecID() { return trecID; }
+      std::string getTargetURI() { return targetURI; }
+      const char *getHeader() { return header.c_str(); }
+      const char *getContent(){ return content; }
+
+      std::string getMetadata(const char *key);
+
+      bool readRecord();
+      // header string constants
+      static const char * WARCTYPE;
+      static const char * WARCRECORDID;
+      static const char * CONTENTLENGTH;
+      static const char * WARCTARGETURI;
+      static const char * WARCTRECID;
+    };
+
     class WARCDocumentIterator : public DocumentIterator {
     private:
+      WARCRecord *_record;
       UnparsedDocument _document;
       gzFile _gzin;
       indri::utility::Buffer _buffer;
       indri::utility::Buffer _metaBuffer;
-      std::string _fileName;
-      const char * _recordID;
-      const char * _contentLength;
-      const char *_targetURI;
-      const char *_trecID;
-      int _recordIDLength;
-      int _contentLengthLength;
-      int _targetURILength;
-      int _trecIDLength;
       std::string _warcUUID;
       const char * _warcMeta;
       const char * _dochdr;
       const char * _docnoString;
       char _docno[512];
-
-      bool _readLine( char*& beginLine, size_t& lineLength );
 
     public:
       WARCDocumentIterator();
--- parsing/src/WARCDocumentIterator.cpp
+++ parsing/src/WARCDocumentIterator.cpp
@@ -19,82 +19,52 @@
 #include "indri/WARCDocumentIterator.hpp"
 #include <iostream>
 #include "Exception.hpp"
-
-indri::parse::WARCDocumentIterator::WARCDocumentIterator() {
-  _gzin = 0;
-  _metaBuffer.grow (512 * 1024);
-  _recordID = 0;
-  _contentLength = 0;
-  _recordIDLength = 0;
-  _contentLengthLength = 0;
-  _warcMeta = "warc";
-  _dochdr = "dochdr";
-  _docnoString = "docno";
-  _recordID = "WARC-Record-ID:";
-  _recordIDLength = (int)strlen(_recordID);
-  _contentLength = "Content-Length:";
-  _contentLengthLength = (int)strlen(_contentLength);
-  _targetURI = "WARC-Target-URI:";
-  _targetURILength = strlen(_targetURI);
-  _trecID = "WARC-TREC-ID:";
-  _trecIDLength = strlen(_trecID);
+// helpers
+#define SPACES " \t\r\n"
+inline std::string trim_right (const string & s, const string & t = SPACES) {
+  std::string d (s);
+  std::string::size_type i (d.find_last_not_of (t));
+  if (i == std::string::npos)
+    return std::string("");
+  else
+    return d.erase (d.find_last_not_of (t) + 1) ;
 }
 
-indri::parse::WARCDocumentIterator::~WARCDocumentIterator() {
-  close();
+inline std::string trim_left (const string & s, const string & t = SPACES) {
+  string d (s);
+  return d.erase (0, s.find_first_not_of (t)) ;
 }
 
-void indri::parse::WARCDocumentIterator::open( const std::string& filename ) {
-  _fileName = filename;
-    _gzin = gzopen(filename.c_str(), "rb");
-  if( !_gzin)
-    LEMUR_THROW( LEMUR_IO_ERROR, "Couldn't open file " + filename + "." );
+inline std::string trim (const string & s, const string & t = SPACES) {
+  string d (s);
+  return trim_left (trim_right (d, t), t) ;
+}
 
-  // Consume the first WARC record (type warcinfo)
-  // should verify the WARC-Type is warcinfo
-  // if not, it's a partial file or broken... bleah.
-  // read until the WARC-Record-ID line.
-  // parse the urn:uri value for the file unique identifier (ugly)
-  // read until the Content-Length line.
-  // parse the length (atoi)
-  // read until an empty line
-  // read length bytes into record
-
-  char* beginLine;
-  size_t lineLength;  
-  bool result;
-  int contentLength = 0;
-  _buffer.clear();
-  do {
-    result = _readLine( beginLine, lineLength );
-    if (! result) break;
-    if (!strncmp(_recordID, beginLine, _recordIDLength)) {
-      // parse out the uuid
-      // strip ' <urn:uuid:' from the front
-      // strip '>\n' from the back.
-      _warcUUID.assign(beginLine + _recordIDLength + 11, 
-                       lineLength - (_recordIDLength + 12 + 2));
-    } else if (!strncmp(_contentLength, beginLine, _contentLengthLength)) {
-      // parse out the content length;
-      contentLength = atoi(beginLine + _contentLengthLength) + 1;
-    }
-  } while (result && (!contentLength || (strlen(beginLine) > 1))) ;
-  
-  if (result) {
-    // consume (and discard) the record.
-    gzseek(_gzin, contentLength, SEEK_CUR);
+inline void split(const std::string &line, std::string &key, 
+                  std::string&value, char delim = ':' ) {
+  std::string::size_type colon = line.find(delim);
+  if (colon != std::string::npos) {
+      key = line.substr(0, colon);
+      value = line.substr(colon+1);
   }
-  // file pointer is now positioned to read the first response record.
 }
 
-void indri::parse::WARCDocumentIterator::close() {
-  if (_gzin)
-    gzclose(_gzin);
-  _gzin = 0;
+
+//
+
+const char * indri::parse::WARCRecord::WARCTYPE = "WARC-Type";
+const char * indri::parse::WARCRecord::WARCRECORDID = "WARC-Record-ID";
+const char * indri::parse::WARCRecord::CONTENTLENGTH = "Content-Length";
+const char * indri::parse::WARCRecord::WARCTARGETURI = "WARC-Target-URI";
+const char * indri::parse::WARCRecord::WARCTRECID = "WARC-TREC-ID";
+
+std::string indri::parse::WARCRecord::getMetadata(const char *key) {
+  std::string *result = metadata.find(key);
+  return result ? *result : std::string("");
 }
 
-bool indri::parse::WARCDocumentIterator::_readLine( char*& beginLine, 
-                                                    size_t& lineLength ) {
+bool indri::parse::WARCRecord::_readLine( char*& beginLine, 
+                                          size_t& lineLength ) {
   lineLength = 0;
   size_t actual;
   // make a buffer of a reasonable size so we're not always allocating
@@ -130,62 +100,178 @@ bool indri::parse::WARCDocumentIterator::_readLine( char*& beginLine,
   return true;
 }
 
+bool indri::parse::WARCRecord::readHeader() {
+  // skip over empty lines until content-length has been found
+  // to accomodate the slightly malformed clueweb warcs.
+  // Consume the first WARC record (type warcinfo)
+  // should verify the WARC-Type is warcinfo
+  // if not, it's a partial file or broken... bleah.
+  // read until the WARC-Record-ID line.
+  // parse the urn:uri value for the file unique identifier (ugly)
+  // read until the Content-Length line.
+  // parse the length (atoi)
+  // read until an empty line
+  // read length bytes into record
+
+  char* beginLine;
+  size_t lineLength;  
+  std::string line;
+  std::string key, value;
+  bool result;
+  bool empty = false;
+  
+  do {
+    result = _readLine( beginLine, lineLength );
+    if (! result) break;
+    line.assign(beginLine, lineLength);
+    // gather the unmolested header string
+    header += line;
+    // strip the newline (CRLF or LF)
+    line = trim_right(line, "\r\n");
+    empty = line.size() == 0;
+    if (! empty) {
+      split(line, key, value);
+      value = trim_left(value);
+      // classify and add to metadata
+      if (key == WARCTYPE) {
+        warcType.assign(value);
+      } else if (key == WARCRECORDID) {
+        // trim '<urn:uuid:' from front, '>' from back
+        uuid.assign(value.substr(10, value.size() - 11));
+      } else if (key == CONTENTLENGTH) {
+        contentLength = atoi(value.c_str());
+      } else if (key == WARCTARGETURI) {
+        targetURI.assign(value);
+      } else if (key == WARCTRECID) {
+        trecID.assign(value);
+      } else {
+        // stuff it in the hash table
+        metadata.insert(key, value);
+      }
+    }
+  } while (result && (!contentLength || !empty)) ;
+  return contentLength != 0;
+}
+
+bool indri::parse::WARCRecord::readContent() {
+  // read they bytes
+  _buffer.clear();
+  content = (char *)_buffer.front();
+  int numRead;
+  numRead = gzread(_gzin, _buffer.write(contentLength), contentLength);
+  if (numRead != contentLength) {
+    return false;
+  }
+  // terminate the string
+  *_buffer.write(1) = 0;
+#if 0
+  // consume two empty lines?
+  // works correctly with heritrix WARC files, fails with
+  // ClueWeb09 nutch-generated files.
+  size_t length;
+  char *beginLine;
+  // test that each line is just a newline (CRLF or LF);
+  bool read = _readLine(beginLine, length);
+  read = _readLine(beginLine, length);
+#endif
+  return true;
+}
+
+bool indri::parse::WARCRecord::readRecord() {
+  metadata.clear();
+  contentLength = 0;
+  header = "";
+  warcType = "";
+  uuid = "";
+  targetURI  = "";
+  trecID = "";
+  bool res = readHeader();
+  if (! res) {
+    return false;
+  }  
+  res = readContent();
+  return res;
+}
+
+indri::parse::WARCRecord::~WARCRecord() {
+}
+
+indri::parse::WARCDocumentIterator::WARCDocumentIterator() {
+  _gzin = 0;
+  _record = 0;
+  _metaBuffer.grow (512 * 1024);
+  _warcMeta = "warc";
+  _dochdr = "dochdr";
+  _docnoString = "docno";
+}
+
+indri::parse::WARCDocumentIterator::~WARCDocumentIterator() {
+  close();
+}
+
+void indri::parse::WARCDocumentIterator::open( const std::string& filename ) {
+    _gzin = gzopen(filename.c_str(), "rb");
+  if( !_gzin)
+    LEMUR_THROW( LEMUR_IO_ERROR, "Couldn't open file " + filename + "." );
+  _record = new WARCRecord(_gzin, _buffer);
+  // Consume the first WARC record (type warcinfo)
+  // verify the WARC-Type is warcinfo
+  // if not, it's a partial file or broken... bleah.
+  bool read = _record->readRecord();
+  if ( ! read ) {
+    LEMUR_THROW(LEMUR_IO_ERROR, "Bad WARC file." );
+  }
+  std::string warcType = _record->getWarcType();
+  if ( warcType != "warcinfo" ) {
+    LEMUR_THROW(LEMUR_IO_ERROR, "Bad WARC file." );
+  }
+  _warcUUID = _record->getUUID();
+  // file pointer is now positioned to read the first response record.
+}
+
+void indri::parse::WARCDocumentIterator::close() {
+  if (_gzin)
+    gzclose(_gzin);
+  _gzin = 0;
+  delete(_record);
+  _record = 0;
+}
+
 indri::parse::UnparsedDocument* indri::parse::WARCDocumentIterator::nextDocument() {
   _document.metadata.clear();
-  _buffer.clear();
   _metaBuffer.clear();
+
   char* beginLine;
   size_t lineLength;  
   bool result;
-
+  // loop until a response record is found
   // Consume the WARC record, put into WARC metadata
-  // read until the WARC-Record-ID line.
-  // parse the urn:uri value for the record unique identifier (ugly)
+  // verify the WARC-Type is response
+  // if not, the record should be skipped
+  // get urn:uri value for the record unique identifier
   // combine with the file uuid to make a DOCNO
-  // should verify the WARC-Type is response
-  // if not, the record should be skipped (are there any?)
-  // read the WARC-Target-URI to get the url for the front of the DOCHDR.
-  // read until the Content-Length line.
-  // parse the length (atoi)
-
-  // read length bytes into content.
-  // mark off the http header
-  // read two newlines
+  // get the WARC-Target-URI to get the url for the front of the DOCHDR.
+  std::string warcType = "";
+  do {
+    result = _record->readRecord();
+    if (! result) return 0;
+    warcType = _record->getWarcType();
+  } while (warcType != "response");
+  // current record is a response
 
   std::string uuid;
   std::string trecDocno = "";
-  int contentLength = 0;
-
   std::string uri;
-  do {
-    result = _readLine( beginLine, lineLength );
-    if (! result) return 0;
-    if (!strncmp(_trecID, beginLine, _trecIDLength)) {
-        // have a trec id to use
-        trecDocno.assign(beginLine + _trecIDLength + 1, 
-                         lineLength - (_trecIDLength + 2));
-
-      } else if (!strncmp(_recordID, beginLine, _recordIDLength)) {
-      // parse out the uuid
-      // strip ' <urn:uuid:' from the front
-      // strip '>\n' from the back.
-      uuid.assign(beginLine + _recordIDLength + 11, 
-                  lineLength - (_recordIDLength + 12 + 2));
-    } else if (! strncmp(_targetURI, beginLine, _targetURILength) ) {
-      // save the url to put into the dochdr field for HTMLParser
-      uri.assign(beginLine + _targetURILength + 1, 
-                 lineLength - (_targetURILength + 1));
-    } else if (!strncmp(_contentLength, beginLine, _contentLengthLength)) {
-      // parse out the content length;
-      contentLength = atoi(beginLine + _contentLengthLength);
-    }
-
-  } while( !contentLength || strlen(beginLine) > 1 );
+  const char *header;
   
+  uuid = _record->getUUID();
+  trecDocno = _record->getTrecID();
+  uri = _record->getTargetURI();
+  header = _record->getHeader();
+  int headerLen = strlen(header);
   // copy whatever we've read so far into the _metaBuffer.
-  memcpy( _metaBuffer.write(_buffer.position()), _buffer.front(), 
-          _buffer.position() * sizeof(char));
-  *_metaBuffer.write(1) = 0; // terminate it.
+  memcpy( _metaBuffer.write(headerLen + 1), header, 
+          (headerLen + 1) * sizeof(char));
 
   // make a WARC metadata tag
   indri::parse::MetadataPair warcMetadata;
@@ -216,41 +302,44 @@ indri::parse::UnparsedDocument* indri::parse::WARCDocumentIterator::nextDocument
   dochdrMetadata.valueLength = uri.size();
 
   // read they bytes
-  int startDocument = (int)_buffer.position() - 1;
-  _buffer.unwrite(1);
-  int numRead;
-  numRead = gzread(_gzin, _buffer.write(contentLength), contentLength);
-    
-  if (numRead != contentLength) {
-    // bad things
-    LEMUR_THROW(LEMUR_IO_ERROR, "Short read." );
-  }
+  const char *startDocument = _record->getContent();
+  int contentLen = strlen(startDocument);
   // copy until the two sequential empty lines into metabuffer
   int newlines = 0;
-  char * header = _buffer.front() + startDocument;
+  int numRead = 0;
+  header = startDocument;
   for ( numRead = 0; 
-        newlines != 2 && (numRead + startDocument)  <_buffer.position();
+        newlines != 2 && (numRead < contentLen );
         numRead++)
-    if (header[numRead] == '\n') newlines++; else newlines = 0;
-
-  if (newlines != 2) {
-    // didn't find a record, so we're done
+    if (header[numRead] == '\n' ) {
+      newlines++; 
+    } else if (header[numRead] == '\r' && header[numRead+1] == '\n') {
+      newlines++; 
+      numRead++;
+    } else {
+      newlines = 0;
+    }
+  if (newlines != 2 && numRead < contentLen ) {
+    // didn't find an http request header.
+    // dns requests do not have one (numRead == contentLen)
     return 0;
   }
-
-  memcpy( _metaBuffer.write(numRead), header, numRead * sizeof(char) );
-  dochdrMetadata.valueLength += numRead;
-
+  if (numRead < contentLen) {
+    // found a header
+    memcpy( _metaBuffer.write(numRead), header, numRead * sizeof(char) );
+    dochdrMetadata.valueLength += numRead;
+  } else {
+    // dns response has none
+    numRead = 0;
+  }
+  
   *_metaBuffer.write(1) = 0; // terminate it.
   dochdrMetadata.valueLength += 1; // add the terminator
   _document.metadata.push_back(dochdrMetadata);
 
-  // terminate the string
-  *_buffer.write(1) = 0;
-
-  _document.content = _buffer.front() + startDocument + numRead;
-  _document.contentLength = _buffer.position() - startDocument - numRead - 1;
-  _document.text = _buffer.front();
-  _document.textLength = _buffer.position();
+  _document.content =  startDocument + numRead;
+  _document.contentLength = contentLen - numRead ;
+  _document.text = startDocument;
+  _document.textLength = contentLen + 1;
   return &_document;
 }
